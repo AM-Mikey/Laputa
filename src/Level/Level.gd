9b485e34ae85a97af22bf95d4e42c1e8dbc753e0
AM-Mikey <zim2143658709@gmail.com>
2021-05-24 14:19:20 -0500

    fixed camera scaling on indoor levels

--- src/Level/Level.gd
+++ src/Level/Level.gd
@@ -3,7 +3,12 @@ extends Node2D
 export var level_name: String
 export var music: String
 
+onready var world = get_tree().get_root().get_node("World")
+
 func _ready():
+	if self.has_node("Camera2D"):
+		$Camera2D.zoom = Vector2(1 / world.resolution_scale, 1 / world.resolution_scale)
+	
 	if self.has_node("Notes"):
 		get_node("Notes").visible = false
 
