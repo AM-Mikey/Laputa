c6635f39b5050c8eee8474b33cb02c50d82cdae4
AM-Mikey <76826262+AM-Mikey@users.noreply.github.com>
2022-03-19 20:04:28 -0500

    Fixed Cursor selection for tiles in editor

--- src/Editor/Tiles.gd
+++ src/Editor/Tiles.gd
@@ -7,11 +7,11 @@ signal multi_erase_toggled(toggle)
 
 var tileset 
 var texture
-var columns
-var rows
+var columns: int
+var rows: int
 
 var hovered_tile
-var selected_tile_region = Vector2.ZERO #Top Left ID, Bottom Right ID
+var selected_tile_region: Rect2 = Rect2(0, 0, 16, 16) #in texture space
 var selected_tiles = []
 
 export var tile_separation: int = 1
@@ -29,18 +29,15 @@ func setup_tileset(new):
 
 
 func setup_tile_buttons():
-	#var buttons = get_node(buttons)
-#	buttons.add_constant_override("separation", tile_separation)
-	
 	for c in get_node(buttons).get_children(): #clear old rows
 		c.free()
-		
-	###########################################################################
+###
 	get_node(buttons).add_constant_override("separation", tile_separation)
 	var r_id = 0
 	for r in rows:
 		var row = HBoxContainer.new()
 		row.add_constant_override("separation", tile_separation)
+		#row.mouse_filter = Control.MOUSE_FILTER_IGNORE
 		get_node(buttons).add_child(row)
 		var c_id = 0
 		for c in columns:
@@ -48,24 +45,7 @@ func setup_tile_buttons():
 			row.add_child(button)
 			c_id += 1
 		r_id += 1
-
-
-################################
-		
-		
-#	for r in rows:
-#		var row = HBoxContainer.new()
-#		row.add_constant_override("separation", tile_separation)
-#		get_node(buttons).add_child(row)
-#		for c in columns:
-#			var column = VBoxContainer.new()
-#			column.add_constant_override("separation", tile_separation)
-#			row.add_child(column)
-		
-####################################################
-
-	#var texture_last_tile_id = floor(columns * rows) -1
-
+###
 	for i in tileset.get_tiles_ids(): # for i in range(0, texture_last_tile_id):
 		var x_pos: int = floor(tileset.tile_get_region(i).position.x/16)
 		var y_pos: int = floor(tileset.tile_get_region(i).position.y/16)
@@ -75,53 +55,11 @@ func setup_tile_buttons():
 		button.texture = get_tile_as_texture(i)
 		button.connect("mouse_entered", self, "hover_tile", [button])
 		button.connect("mouse_exited", self, "unhover")
+#		button.get_child(0).connect("mouse_entered", self, "hover_tile", [button])
+#		button.get_child(0).connect("mouse_exited", self, "unhover")
+		
+#		button.connect("gui_input", self, "button_input", [button])
 
-
-
-#########################
-
-# OLD
-#	get_node(buttons).add_constant_override("separation", tile_separation)
-#
-#	var r_id = 0
-#	for r in rows:
-#		var row = HBoxContainer.new()
-#		row.add_constant_override("separation", tile_seperation)
-#		get_node(buttons).add_child(row)
-#
-#		var c_id = 0
-#		for x in columns:
-#			var tile = load("res://src/Editor/TileButton.tscn").instance()
-#			var sp_tex = AtlasTexture.new()
-#			sp_tex.atlas = texture
-#			sp_tex.region = Rect2(c_id * 16, r_id * 16, 16, 16)
-#
-#			tile.id = r_id * columns + c_id
-#			tile.texture = sp_tex
-#			row.add_child(tile)
-#			tile.connect("mouse_entered", self, "hover_tile", [tile])
-#			tile.connect("mouse_exited", self, "unhover")
-#			c_id +=1
-#
-#		r_id += 1
-	
-	
-	
-	
-	
-#OLD OLD
-#	tileset.clear()
-#
-#	var id = tileset.get_last_unused_tile_id()
-#	while id < rows * columns:
-#		tileset.create_tile(id)
-#		tileset.tile_set_texture(id, texture)
-#
-#		var x_pos = (id % columns) * 16
-#		var y_pos = floor(id / columns) * 16
-#		var region = Rect2(x_pos, y_pos, 16, 16)
-#		tileset.tile_set_region(id, region)
-#		id += 1
 
 func get_tile_as_texture(id) -> Texture:
 	var tile_texture = AtlasTexture.new()
@@ -129,40 +67,62 @@ func get_tile_as_texture(id) -> Texture:
 	tile_texture.region = tileset.tile_get_region(id)
 	return tile_texture
 
-
+#func button_input(event, button):
+#	if event.is_action_pressed("editor_lmb"):
+#		print("started ", button.id)
+#		selected_tile_region = tileset.tile_get_region(button.id)
+#		selected_tiles.clear()
+#		return
+#	elif event.is_action_released("editor_lmb"):
+#		print("ended ", button.id)
+#		selected_tile_region.expand(tileset.tile_get_region(button.id).position + Vector2(16, 16))
+#		set_selection()
+#	else:
+#		return
 
 func hover_tile(tile):
 	hovered_tile = tile.id
+	#print("hovered: ", hovered_tile)
 func unhover():
 	hovered_tile = null
 
-
-
+#
+#
 func _input(event):
 	if event.is_action_pressed("editor_lmb") and hovered_tile:
-		#print("started ", hovered_tile)
-		selected_tile_region.x = hovered_tile
-	
-	if event.is_action_released("editor_lmb") and hovered_tile:
-		#print("ended ", hovered_tile)
-		selected_tile_region.y = hovered_tile
-		set_cursor()
-		emit_signal("tile_selection_updated", selected_tile_region)
+		print("started ", hovered_tile)
+		selected_tile_region = tileset.tile_get_region(hovered_tile)
+		selected_tiles.clear()
 
 
+	if event.is_action_released("editor_lmb"):
+		yield(get_tree(), "idle_frame")
+		print("ended ", hovered_tile)
+		if hovered_tile:
+			selected_tile_region = selected_tile_region.expand(tileset.tile_get_region(hovered_tile).position + Vector2(16, 16)) #TODO expand is not working
+			set_selection()
+
+
+
+
+
+func set_selection():
+	set_cursor()
+	for i in tileset.get_tiles_ids():
+		if selected_tile_region.encloses(tileset.tile_get_region(i)):
+			selected_tiles.append(i)
+	print("region: ", selected_tile_region)
+	print("selected: ", selected_tiles)
+	#emit_signal("tile_selection_updated", selected_tiles)
 
 func set_cursor():
-	var start_id = int(min(selected_tile_region.x, selected_tile_region.y))
-	var end_id = int(max(selected_tile_region.x, selected_tile_region.y))
-	var x0 = (start_id % columns) * 17
-	var y0 = floor(start_id / columns) * 17
-	var x1 = (end_id % columns+1) * 17
-	var y1 = floor(end_id / columns+1) * 17
-	var dx = x1 - x0
-	var dy = y1 - y0
-	get_node(cursor).rect_position = Vector2(x0, y0)
-	get_node(cursor).rect_size = Vector2(dx, dy)
-
+	var x_pos = selected_tile_region.position.x + (floor(selected_tile_region.position.x / 16) * tile_separation)
+	var y_pos = selected_tile_region.position.y + (floor(selected_tile_region.position.y / 16) * tile_separation)
+	get_node(cursor).rect_position = Vector2(x_pos, y_pos) 
+	var x_size = selected_tile_region.size.x + (floor(selected_tile_region.size.x / 16) * tile_separation)
+	var y_size = selected_tile_region.size.y + (floor(selected_tile_region.size.y / 16) * tile_separation)
+	get_node(cursor).rect_size = Vector2(x_size, y_size) 
+	
 
 
 func _on_AutoLayer_toggled(button_pressed):
