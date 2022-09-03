8d91df562458c2ded6811550393843e607c6400e
AM-Mikey <76826262+AM-Mikey@users.noreply.github.com>
2022-09-03 10:06:55 -0500

    Revert "reimport"
    This reverts commit 9cea75b2c0fb04007a96e1a5d58410139ffce7aa.

--- src/Gun/TurnstileJumper.gd
+++ src/Gun/TurnstileJumper.gd
@@ -5,7 +5,8 @@ func _ready():
 	description = "A powerful pistol once wielded by a lone cowboy."
 	texture = load("res://assets/Gun/TurnstileJumper.png")
 	icon_texture = load("res://assets/Gun/TurnstileJumperIcon.png")
-	sfx = "gun_revolver"
+	sfx = "gun_pistol"
+	cooldown_time = 0.1
 	automatic = false
 	max_ammo = 0
 	max_level = 3
@@ -14,26 +15,22 @@ func _ready():
 func load_level():
 	match level:
 		1:
-			bullet_scene = load("res://src/Bullet/TurnstileJumper1.tscn")
-			damage = 2
-			f_range = 100
+			bullet_scene = load("res://src/Bullet/BulletRevolver1.tscn")
+			f_range = 60
 			speed = 256
 			max_xp = 10
-			cooldown_time = 0.4
 		2:
-			bullet_scene = load("res://src/Bullet/TurnstileJumper2.tscn")
-			damage = 4
-			f_range = 150
+			bullet_scene = load("res://src/Bullet/BulletRevolver2.tscn")
+			damage = 2
+			f_range = 96
 			speed = 320
 			max_xp = 15
-			cooldown_time = 0.6
 		3:
-			bullet_scene = load("res://src/Bullet/TurnstileJumper3.tscn")
-			damage = 6
-			f_range = 200
+			bullet_scene = load("res://src/Bullet/BulletRevolver3.tscn")
+			damage = 4
+			f_range = 128
 			speed = 384
 			max_xp = 20
-			cooldown_time = 1.0
 
 func activate():
 	var origin = pc.get_node("BulletOrigin").global_position
