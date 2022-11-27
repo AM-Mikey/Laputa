ed7ba5f7f8e3f5e3cefc199011f75f32f2b33728
AM-Mikey <76826262+AM-Mikey@users.noreply.github.com>
2022-11-26 18:34:10 -0600

    updated editor icons

--- src/Editor/TabButtons.gd
+++ src/Editor/TabButtons.gd
@@ -2,6 +2,8 @@ extends Control
 
 const TAB_BUTTON = preload("res://src/Editor/Button/TabButton.tscn")
 
+var icons = load("res://assets/Editor/TabIcons.png")
+
 onready var editor = get_parent().get_parent()
 onready var tabs = get_parent().get_node("Tab").get_children()
 
@@ -9,7 +11,12 @@ func _ready():
 	var index = 0
 	for t in tabs:
 		var button = TAB_BUTTON.instance()
-		button.icon = load(t.icon)
+		
+		var icon = AtlasTexture.new()
+		icon.atlas = icons
+		icon.region = Rect2(16 * index, 0, 16, 16)
+		button.icon = icon
+		
 		button.tab_index = index
 		$VBox.add_child(button)
 		button.connect("tab_selected", editor, "on_tab_selected")
