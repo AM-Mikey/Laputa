67d72c819a0d71fb03aa998697c6b667ba622ca9
Alex Noir <varsash@gmail.com>
2025-08-21 21:54:03 +0300

    Get LevelSelect to work by removing outdated code (feel free to revert)

--- src/UI/LevelSelect/LevelSelect.gd
+++ src/UI/LevelSelect/LevelSelect.gd
@@ -9,8 +9,6 @@ func _ready(): #TODO: update load and save to new file
 	on_viewport_size_changed()
 	
 	var nones = []
-	var coasts = []
-	var villages= []
 	
 	var level_dir = DirAccess.open("res://src/Level")
 	level_dir.list_dir_begin() # TODOConverter3To4 fill missing arguments https://github.com/godotengine/godot/pull/40547
@@ -23,57 +21,12 @@ func _ready(): #TODO: update load and save to new file
 				nones.append("res://src/Level/%s" %file)
 	level_dir.list_dir_end()
 	
-	var demo_dir = DirAccess.open("res://src/Level/Coast")
-	demo_dir.list_dir_begin() # TODOConverter3To4 fill missing arguments https://github.com/godotengine/godot/pull/40547
-	while true:
-		var file = demo_dir.get_next()
-		if file == "":
-			break
-		elif not file.begins_with("."):
-			if file.ends_with(".tscn"):
-				coasts.append("res://src/Level/Coast/%s" %file)
-	demo_dir.list_dir_end()
-	
-	demo_dir.open("res://src/Level/Village")
-	demo_dir.list_dir_begin() # TODOConverter3To4 fill missing arguments https://github.com/godotengine/godot/pull/40547
-	while true:
-		var file = demo_dir.get_next()
-		if file == "":
-			break
-		elif not file.begins_with("."):
-			if file.ends_with(".tscn"):
-				villages.append("res://src/Level/Village/%s" %file)
-	demo_dir.list_dir_end()
-	
-#	demo_dir.open("res://src/Level/CloudFactory")
-#	demo_dir.list_dir_begin()
-#	while true:
-#		var file = demo_dir.get_next()
-#		if file == "":
-#			break
-#		elif not file.begins_with("."):
-#			if file.ends_with(".tscn"):
-#				nones.append("res://src/Level/CloudFactory/%s" %file)
-#	demo_dir.list_dir_end()
-
 	for n in nones:
 		if n.rfind(".tscn"):
 			var level_button = LEVELBUTTON.instantiate()
 			level_button.level = n
 			$MarginContainer/ScrollContainer/VBox/HBox/None/VBox.add_child(level_button)
 	
-	for c in coasts:
-		if c.rfind(".tscn"):
-			var level_button = LEVELBUTTON.instantiate()
-			level_button.level = c
-			$MarginContainer/ScrollContainer/VBox/HBox/Coast/VBox.add_child(level_button)
-		
-	for v in villages:
-		if v.rfind(".tscn"):
-			var level_button = LEVELBUTTON.instantiate()
-			level_button.level = v
-			$MarginContainer/ScrollContainer/VBox/HBox/Village/VBox.add_child(level_button)
-
 	########
 	var first_button = $MarginContainer/ScrollContainer/VBox/HBox/None/VBox.get_child(0)
 	
