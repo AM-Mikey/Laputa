4d7e63f7226122945ea547595b988d726cb115f9
AM-Mikey <76826262+AM-Mikey@users.noreply.github.com>
2025-10-01 23:00:28 -0500

    run state animation overhaul part 1

--- src/Player/Run.gd
+++ src/Player/Run.gd
@@ -84,10 +84,10 @@ func check_shoot_down() -> bool:
 	return out
 
 func animate():
-	sprite.position = Vector2i(0.0, -16.0)
-	sprite.gun_pos_offset = Vector2.ZERO
-	var animation = "run"
-	var reference_texture = preload("res://assets/Player/Run.png")
+	#sprite.position = Vector2i(0.0, -16.0)
+	#sprite.gun_pos_offset = Vector2.ZERO
+	var animation = []
+	#var reference_texture
 	
 	var absolute_left = pc.get_node("AbsoluteLeft").get_collider()
 	var absolute_right = pc.get_node("AbsoluteRight").get_collider()
@@ -103,91 +103,264 @@ func animate():
 
 	#var edge_connections = ["edge_turn", "run"]
 	#var edge_turn_connections = ["edge", "run"]
-	var connected_animations = {
-		"edge": ["edge_turn", "run"],
-		"edge_turn": ["edge", "run"],
-		"edge_front": ["edge", "run"],
-		"push": ["stand", "up_slope", "down_slope", "slight_up_slope", "slight_down_slope"],
-	}
-	
-	if pc.direction_lock != Vector2i.ZERO and pc.direction_lock.x != sign(pc.move_dir.x):
-		animation = "back_run"
-		reference_texture = preload("res://assets/Player/BackRun.png")
-	if pc.move_dir.x == 0.0 and saved_move_dir.x == 0.0: #standing
-		
-		if (!absolute_left and !absolute_right): #only a middle colliding
-				do_edge_turn = false
-				sprite.position = Vector2i(0.0, -15.0) #1 down
-				animation = "stand_peak"
-				reference_texture = preload("res://assets/Player/StandPeak.png")
-		elif (!absolute_left and absolute_right and slight_slope_left) or (!absolute_right and absolute_left and slight_slope_right):
-			do_edge_turn = false
-			sprite.position = Vector2i(0.0, -12.0)
-			if (!absolute_left and pc.look_dir.x == 1.0) or (!absolute_right and pc.look_dir.x == -1.0):
-				animation = "slight_up_slope"
-				reference_texture = preload("res://assets/Player/SlightUpSlope.png")
-			else:
-				animation = "slight_down_slope"
-				reference_texture = preload("res://assets/Player/SlightDownSlope.png")
-		elif (!absolute_left and absolute_right and slope_left) or (!absolute_right and absolute_left and slope_right):
-			do_edge_turn = false
-			if (!absolute_left and pc.look_dir.x == 1.0) or (!absolute_right and pc.look_dir.x == -1.0):
-				sprite.position = Vector2i(0.0, -7.0)
-				animation = "up_slope"
-				reference_texture = preload("res://assets/Player/UpSlope.png")
-			else:
-				sprite.position = Vector2i(0.0, -9.0)
-				animation = "down_slope"
-				reference_texture = preload("res://assets/Player/DownSlope.png")
-		elif (!edge_left and absolute_right and pc.look_dir.x == 1.0) or (!edge_right and absolute_left and pc.look_dir.x == -1.0):
-			if do_edge_turn:
-				animation = "edge_turn"
-				reference_texture = preload("res://assets/Player/EdgeTurn.png")
-			else:
-				animation = "edge_front"
-				reference_texture = preload("res://assets/Player/EdgeFront.png")
-		elif (!stand_close_left and absolute_right and pc.look_dir.x == 1.0) or (!stand_close_right and edge_left and pc.look_dir.x == -1.0):
-			animation = "stand_peak"
-			reference_texture = preload("res://assets/Player/StandPeak.png")
-		elif (!edge_left and pc.look_dir.x == -1.0) or (!edge_right and pc.look_dir.x == 1.0):
-			do_edge_turn = true
-			animation = "edge"
-			reference_texture = preload("res://assets/Player/Edge.png")
-		elif (!stand_close_left and pc.look_dir.x == -1.0) or (!stand_close_right and pc.look_dir.x == 1.0):
-			do_edge_turn = false
-			animation = "stand_close"
-			reference_texture = preload("res://assets/Player/StandClose.png")
-		else:
-			do_edge_turn = false
-			animation = "stand"
-			reference_texture = preload("res://assets/Player/Stand.png")
-	elif push_left_wall or push_right_wall:
-		animation = "push"
-		reference_texture = preload("res://assets/Player/Push.png")
-	else: #running
-		if (!absolute_left and absolute_right and slight_slope_left) or (!absolute_right and absolute_left and slight_slope_right): #run on slight slope
-			sprite.position = Vector2i(0.0, -13.0)
-			sprite.gun_pos_offset = Vector2(0, 3)
-		elif (!absolute_left and absolute_right and slope_left) or (!absolute_right and absolute_left and slope_right): #run on slope
-			sprite.position = Vector2i(0.0, -12.0)
-			sprite.gun_pos_offset = Vector2(0, 4)
+	#var connected_animations = {
+		#"edge": ["edge_turn", "run"],
+		#"edge_turn": ["edge", "run"],
+		#"edge_front": ["edge", "run"],
+		#"push": ["stand", "up_slope", "down_slope", "slight_up_slope", "slight_down_slope"],
+	#}
 	
 	
-	if pc.is_crouching:
-		if pc.move_dir.x == 0.0 and saved_move_dir.x == 0.0:
-			animation = "crouch"
-			reference_texture = preload("res://assets/Player/Crouch.png")
-		elif abs(pc.velocity.x) <= 5.0 and pc.is_on_floor():
-			animation = "push"
-			reference_texture = preload("res://assets/Player/Push.png")
-		else:
-			animation = "crouch_run"
-			reference_texture = preload("res://assets/Player/CrouchRun.png")
+	var look_left = pc.look_dir.x == -1.0
+	var look_right = pc.look_dir.x == 1.0
+	
+	var edge_condition = (!absolute_left and !edge_left and !slight_slope_right and !slope_right and absolute_right and look_left) or (absolute_left and !slope_left and !slight_slope_left and !edge_right and !absolute_left and look_right)
+	var stand_condition = (stand_close_left and absolute_right and look_left) or (absolute_left and stand_close_right and look_right) or (absolute_left and edge_right and look_left) or (edge_left and absolute_right and look_right)
+	
+	var slight_up_condition = (absolute_left and slight_slope_left and !edge_left and !absolute_right and look_left) or (!absolute_left and !edge_right and slight_slope_right and absolute_right and look_right)
+	var slight_down_condition = (!absolute_left and !edge_right and slight_slope_right and absolute_right and look_left) or (absolute_left and slight_slope_left and !edge_left and !absolute_right and look_right)
+	var up_condition = (absolute_left and slope_left and !edge_left and !absolute_right and look_left) or (!absolute_left and !edge_right and slope_right and absolute_right and look_right)
+	var down_condition = (!absolute_left and !edge_right and slope_right and absolute_right and look_left) or (absolute_left and slope_left and !edge_left and !absolute_right and look_right)
+	
+	match ap.current_animation: #do not use elif here
+		"edge":
+			if edge_condition:
+				animation.append("edge")
+			if (!edge_right and absolute_left and look_left) or (!edge_left and absolute_right and look_right):
+				animation.append("edge_turn")
+			if (edge_left and !stand_close_left and look_left) or (edge_right and !stand_close_right and look_right):
+				animation.append("stand_close")
+			
+			if pc.move_dir.x != 0.0 and animation == []: #last case
+				animation.append("run")
 
+		"edge_turn":
+			if edge_condition:
+				animation.append("edge")
+			if (!stand_close_right and absolute_left and look_left) or (!stand_close_left and absolute_right and look_right):
+				animation.append("edge_turn")
+			if (edge_left and !stand_close_left and look_left) or (edge_right and !stand_close_right and look_right):
+				animation.append("stand_close")
+			if stand_close_left and stand_close_right: #exception to condition
+				animation.append("stand")
+			
+			
+			if pc.move_dir.x != 0.0 and animation == []: #last case
+				animation.append("run")
+		"stand":
+			if (edge_left and !stand_close_left and look_left) or (edge_right and !stand_close_right and look_right):
+				animation.append("stand_close")
+			
+			if pc.move_dir.x != 0.0 and animation == []: #last case... add room for velocity 
+				if push_left_wall or push_right_wall:
+					animation.append("push")
+				else:
+					animation.append("run")
+		
+		"stand_close":
+			if stand_condition:
+				animation.append("stand") #TODO: when this goes to stand it goes right to run next frame if turn to make this happen, before returning to stand.
+			if (edge_left and !stand_close_left and look_left) or (edge_right and !stand_close_right and look_right):
+				animation.append("stand_close")
+			if edge_condition:
+				animation.append("edge")
+			
+			if pc.move_dir.x != 0.0 and animation == []: #last case
+				animation.append("run")
+		
+		"push":
+			if pc.move_dir.x == 0.0: #not moving
+				animation.append("stand")
+	
+		"run":
+			if pc.move_dir.x == 0.0: #not moving
+				if (edge_left and absolute_right and !stand_close_left and look_left) or (absolute_left and edge_right and !stand_close_right and look_right):
+					animation.append("stand_close")
+				if edge_condition:
+					animation.append("edge")
+				if stand_condition:
+					animation.append("stand")
+				if !absolute_left and !absolute_right:
+					animation.append("stand_peak")
+				if slight_up_condition:
+					animation.append("slight_up_slope")
+				if slight_down_condition:
+					animation.append("slight_down_slope")
+				if up_condition:
+					animation.append("up_slope")
+				if down_condition:
+					animation.append("down_slope")
+			else:
+				if push_left_wall or push_right_wall:
+					if slight_up_condition:
+						animation.append("slight_up_push")
+					elif slight_down_condition:
+						animation.append("slight_down_push")
+					elif up_condition:
+						animation.append("up_push")
+					elif down_condition:
+						animation.append("down_push")
+					else:
+						animation.append("push")
+		
+		"stand_peak":
+			if pc.move_dir.x != 0.0 and animation == []: #last case
+				animation.append("run")
+		
+		"slight_up_slope":
+			if pc.move_dir.x != 0.0 and animation == []: #last case
+				if push_left_wall or push_right_wall:
+					animation.append("slight_up_push")
+				else:
+					animation.append("run")
+		
+		"slight_down_slope":
+			if pc.move_dir.x != 0.0 and animation == []: #last case
+				if push_left_wall or push_right_wall:
+					animation.append("slight_down_push")
+				else:
+					animation.append("run")
+		
+		"up_slope":
+			if pc.move_dir.x != 0.0 and animation == []: #last case
+				if push_left_wall or push_right_wall:
+					animation.append("up_push")
+				else:
+					animation.append("run")
+		
+		"down_slope":
+			if pc.move_dir.x != 0.0 and animation == []: #last case
+				if push_left_wall or push_right_wall:
+					animation.append("down_push")
+				else:
+					animation.append("run")
+
+		"slight_up_push":
+			if pc.move_dir.x == 0.0: #not moving
+				animation.append("slight_up_slope")
+		
+		"slight_down_push":
+			if pc.move_dir.x == 0.0: #not moving
+				animation.append("slight_down_slope")
+
+		"up_push":
+			if pc.move_dir.x == 0.0: #not moving
+				animation.append("up_slope")
+		
+		"down_push":
+			if pc.move_dir.x == 0.0: #not moving
+				animation.append("down_slope")
+	
+	#if pc.direction_lock != Vector2i.ZERO and pc.direction_lock.x != sign(pc.move_dir.x):
+		#animation = "back_run"
+		#reference_texture = preload("res://assets/Player/BackRun.png")
+	#if pc.move_dir.x == 0.0 and saved_move_dir.x == 0.0: #standing
+		#
+		#if (!absolute_left and !absolute_right): #only a middle colliding
+				#do_edge_turn = false
+				#sprite.position = Vector2i(0.0, -15.0) #1 down
+				#animation = "stand_peak"
+				#reference_texture = preload("res://assets/Player/StandPeak.png")
+		#elif (!absolute_left and absolute_right and slight_slope_left) or (!absolute_right and absolute_left and slight_slope_right):
+			#do_edge_turn = false
+			#sprite.position = Vector2i(0.0, -12.0)
+			#if (!absolute_left and pc.look_dir.x == 1.0) or (!absolute_right and pc.look_dir.x == -1.0):
+				#animation = "slight_up_slope"
+				#reference_texture = preload("res://assets/Player/SlightUpSlope.png")
+			#else:
+				#animation = "slight_down_slope"
+				#reference_texture = preload("res://assets/Player/SlightDownSlope.png")
+		#elif (!absolute_left and absolute_right and slope_left) or (!absolute_right and absolute_left and slope_right):
+			#do_edge_turn = false
+			#if (!absolute_left and pc.look_dir.x == 1.0) or (!absolute_right and pc.look_dir.x == -1.0):
+				#sprite.position = Vector2i(0.0, -7.0)
+				#animation = "up_slope"
+				#reference_texture = preload("res://assets/Player/UpSlope.png")
+			#else:
+				#sprite.position = Vector2i(0.0, -9.0)
+				#animation = "down_slope"
+				#reference_texture = preload("res://assets/Player/DownSlope.png")
+		#elif (!edge_left and absolute_right and pc.look_dir.x == 1.0) or (!edge_right and absolute_left and pc.look_dir.x == -1.0):
+			#if do_edge_turn:
+				#animation = "edge_turn"
+				#reference_texture = preload("res://assets/Player/EdgeTurn.png")
+			#else:
+				#animation = "edge_front"
+				#reference_texture = preload("res://assets/Player/EdgeFront.png")
+		#elif (!stand_close_left and absolute_right and pc.look_dir.x == 1.0) or (!stand_close_right and edge_left and pc.look_dir.x == -1.0):
+			#animation = "stand_peak"
+			#reference_texture = preload("res://assets/Player/StandPeak.png")
+		#elif (!edge_left and pc.look_dir.x == -1.0) or (!edge_right and pc.look_dir.x == 1.0):
+			#do_edge_turn = true
+			#animation = "edge"
+			#reference_texture = preload("res://assets/Player/Edge.png")
+		#elif (!stand_close_left and pc.look_dir.x == -1.0) or (!stand_close_right and pc.look_dir.x == 1.0):
+			#do_edge_turn = false
+			#animation = "stand_close"
+			#reference_texture = preload("res://assets/Player/StandClose.png")
+		#else:
+			#do_edge_turn = false
+			#animation = "stand"
+			#reference_texture = preload("res://assets/Player/Stand.png")
+	#elif push_left_wall or push_right_wall:
+		#animation = "push"
+		#reference_texture = preload("res://assets/Player/Push.png")
+	#else: #running
+		#if (!absolute_left and absolute_right and slight_slope_left) or (!absolute_right and absolute_left and slight_slope_right): #run on slight slope #TODO: reimplement this position change!!!
+			#sprite.position = Vector2i(0.0, -13.0)
+			#sprite.gun_pos_offset = Vector2(0, 3)
+		#elif (!absolute_left and absolute_right and slope_left) or (!absolute_right and absolute_left and slope_right): #run on slope
+			#sprite.position = Vector2i(0.0, -12.0)
+			#sprite.gun_pos_offset = Vector2(0, 4)
+	#
+	#
+	#if pc.is_crouching:
+		#if pc.move_dir.x == 0.0 and saved_move_dir.x == 0.0:
+			#animation = "crouch"
+			#reference_texture = preload("res://assets/Player/Crouch.png")
+		#elif abs(pc.velocity.x) <= 5.0 and pc.is_on_floor():
+			#animation = "push"
+			#reference_texture = preload("res://assets/Player/Push.png")
+		#else:
+			#animation = "crouch_run"
+			#reference_texture = preload("res://assets/Player/CrouchRun.png")
+
+
+	if animation.is_empty():
+		play_animation(ap.current_animation)
+		return
+	if animation.size() != 1:
+		printerr("ERROR: CONFLICT SETTING RUN ANIMATION FROM ", ap.current_animation, " to ", animation)
+		return
+	#if animation[0] == ap.current_animation:
+		#return
+	do_animation_sprite_offset(animation[0])
+	play_animation(animation[0])
+	
+	saved_move_dir = pc.move_dir #for 2 frame stand
+
+func do_animation_sprite_offset(animation):
+	var offsets = {
+		"stand_peak": Vector2(0, 1),
+		"slight_up_slope": Vector2(0, 4),
+		"slight_down_slope": Vector2(0, 4),
+		"up_slope": Vector2(0, 9),
+		"down_slope": Vector2(0, 7),
+		"slight_up_push": Vector2(0, 6),
+		"slight_down_push": Vector2(0, 6),
+		"up_push": Vector2(0, 9),
+		"down_push": Vector2(0, 8),
+	}
+	sprite.position = Vector2i(0.0, -16.0)
+	if offsets.has(animation):
+		sprite.position += offsets[animation]
+
+
+func play_animation(animation):
 	#for runtime, set the frame counts before the animation starts
+	var reference_texture = load("res://assets/Player/" + animation.to_pascal_case() + ".png")
 	sprite.hframes = int(reference_texture.get_width() / 32.0)
 	sprite.vframes = int(reference_texture.get_height() / 32.0)
-
 	ap.speed_scale = 1.0
 	var do_blending = false
 	
@@ -222,8 +395,8 @@ func animate():
 		ap.play(animation, blend_time, 1.0)
 		if blend_time != 0.0:
 			ap.seek(blend_time, true)
-	
-	saved_move_dir = pc.move_dir #for 2 frame stand
+
+
 
 
 
@@ -286,6 +459,7 @@ func enter(): #TODO: consider setting these back after exiting or just set these
 	pc.floor_max_angle = 0.8 #well over 45degrees so we can have a lower safe_margin
 	pc.safe_margin = 0.008 #may cause issues this low
 	do_edge_turn = false
+	play_animation("run")
 func exit():
 	sprite.position = Vector2i(0.0, -16.0)
 	is_dropping = false
