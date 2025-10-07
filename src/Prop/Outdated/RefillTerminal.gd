76c44c051f793a075574e5a3cc4abb9346968b8c
Alex Noir <varsash@gmail.com>
2025-10-07 18:36:03 +0300

    Get rid of all the "Outdated" folders, except those that are still used

--- src/Prop/Outdated/RefillTerminal.gd
+++
@@ -1,9 +0,0 @@
-extends Prop
-
-func activate():
-	if active_pc.hp < active_pc.max_hp:
-		active_pc.hp = active_pc.max_hp
-		active_pc.emit_signal("hp_updated", active_pc.hp, active_pc.max_hp)
-		am.play("hp_refill")
-	else:
-		am.play("ui_deny")
