c3f1b9bf0a74dbb378194c519eaccac8cfbee0ea
SanpoNeko <sanponeko23@gmail.com>
2025-11-04 12:52:04 +0700

    Fix player camera bug

--- src/Player/PlayerCamera.gd
+++ src/Player/PlayerCamera.gd
@@ -10,8 +10,8 @@ var homing_camera = false
 @export var v_pan_time = 1.5
 @export var h_pan_delay = 0.0
 @export var v_pan_delay = 0.0
-@export var h_pan_distance = 2.0
-@export var v_pan_distance = 2.0
+@export var h_pan_distance = 1.0
+@export var v_pan_distance = 1.0
 
 @onready var w = get_tree().get_root().get_node("World")
 @onready var pc = get_parent()
@@ -32,22 +32,22 @@ func _physics_process(_delta):
 		h_tween.set_speed_scale(max(abs(pc.velocity.x)/mm.speed.x, h_pan_min_speed))
 
 	if !pc.disabled and pc.can_input and !pc.mm.current_state == pc.mm.states["inspect"]:
-		if inp.pressed("look_up",1) or inp.pressed("look_down",1) \
-		or inp.released("look_up") or inp.released("look_down"):
-			pan_vertical(get_v_dir())
+		if Input.is_action_just_pressed("look_up") or Input.is_action_just_pressed("look_down") \
+		or Input.is_action_just_released("look_up") or Input.is_action_just_released("look_down"):
+				pan_vertical(get_v_dir())
 
 
 ### MAIN ###
 
 func pan_vertical(dir):
-	var dist = v_pan_distance / vs.resolution_scale
+	var dist = v_pan_distance
 	if v_tween:
 		v_tween.kill()
 	v_tween = create_tween()
 	v_tween.tween_property(self, "drag_vertical_offset", dir * dist, v_pan_time).set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT).set_delay(v_pan_delay)
 
 func pan_horizontal(dir):
-	var dist = h_pan_distance / vs.resolution_scale
+	var dist = h_pan_distance
 	if h_tween:
 		h_tween.kill()
 	h_tween = create_tween()
@@ -61,8 +61,8 @@ func stop_tween():
 func get_v_dir() -> int:
 	var dir = 0
 	if pc.can_input:
-		if inp.held("look_up"): dir -= 1
-		if inp.held("look_down"): dir += 1
+		if Input.is_action_pressed("look_up"): dir -= 1
+		if Input.is_action_pressed("look_down"): dir += 1
 	return dir
 
 ### TRIGGERS ###
@@ -121,3 +121,6 @@ func spawn_black_bar(bar_name, size, bar_position):
 
 func _resolution_scale_changed(resolution_scale):
 	zoom = Vector2(resolution_scale, resolution_scale)
+	drag_left_margin = 0.75 / resolution_scale - 0.1
+	drag_right_margin = drag_left_margin
+	pan_horizontal(pc.look_dir.x)
