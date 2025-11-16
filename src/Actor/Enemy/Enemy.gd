917f640781e6e93a8e7602dc5a3514860c09460a
AM-Mikey <76826262+AM-Mikey@users.noreply.github.com>
2025-11-16 15:31:08 -0600

    Reworked how top of collision shape is found

--- src/Actor/Enemy/Enemy.gd
+++ src/Actor/Enemy/Enemy.gd
@@ -154,7 +154,13 @@ func _on_hit(damage, blood_direction): #inhereted for enemies to do something on
 ### DAMAGE NUMBER ###
 
 func set_damagenum(damage):
-	var y_offset = $Sprite2D.get_rect().position.y - 2
+	var enabled_col_shapes = []
+	for c in get_children():
+		if c is CollisionShape2D or c is CollisionPolygon2D:
+			if c.disabled == false:
+				enabled_col_shapes.append(c)
+	var good_collision_shape = enabled_col_shapes.front()
+	var y_offset = good_collision_shape.shape.get_rect().position.y - (good_collision_shape.shape.get_rect().size.y / 2.0) #Warning! this assumes that the rect2 is always centered
 
 	if damage_number == null:
 		damage_number = DAMAGE_NUMBER.instantiate()
