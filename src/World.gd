95e46ee322086c25c064992e79aa7a8a7d5ed8e9
SanpoNeko <sanponeko23@gmail.com>
2025-11-17 11:40:36 +0700

    Fix camera no easing after load scene

--- src/World.gd
+++ src/World.gd
@@ -135,7 +135,6 @@ func first_time_level_setup():
 	#wipe would go here if we want one
 	display_level_text(current_level)
 	SaveSystem.read_level_data_from_temp(current_level)
-	await get_tree().process_frame
 	$Juniper/PlayerCamera.position_smoothing_enabled = true
 
 
@@ -170,6 +169,8 @@ func change_level_via_code(level_path):
 	#wipe would go here if we want one
 	display_level_text(current_level)
 	SaveSystem.read_level_data_from_temp(current_level)
+
+	$Juniper/PlayerCamera.position_smoothing_enabled = true
 
 
 func change_level_via_trigger(level_path, door_index):
