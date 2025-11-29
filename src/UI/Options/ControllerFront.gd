a8bf586d0b93e27dd79da1aca3406b49d8d28bf2
AM-Mikey <76826262+AM-Mikey@users.noreply.github.com>
2025-11-28 19:31:16 -0600

    added stick animations

--- src/UI/Options/ControllerFront.gd
+++ src/UI/Options/ControllerFront.gd
@@ -1,5 +1,7 @@
 extends TextureRect
 
+@onready var stick_left_home_pos = %StickLeft.global_position
+@onready var stick_right_home_pos = %StickRight.global_position
 
 #func _physics_process(_delta):
 	#
@@ -20,6 +22,11 @@ func _input(event):
 				%FaceButtonDown.frame = button_frame
 			JOY_BUTTON_DPAD_LEFT, JOY_BUTTON_DPAD_RIGHT, JOY_BUTTON_DPAD_UP, JOY_BUTTON_DPAD_DOWN:
 				display_d_pad()
+	if event is InputEventJoypadMotion:
+		match event.axis:
+			JOY_AXIS_LEFT_X, JOY_AXIS_LEFT_Y: display_stick(%StickLeft)
+			JOY_AXIS_RIGHT_X, JOY_AXIS_RIGHT_Y: display_stick(%StickRight)
+
 
 func display_d_pad():
 	var strength_x = 0
@@ -45,3 +52,52 @@ func display_d_pad():
 		Vector2i(-1, 1): d_pad_frame = 7
 		Vector2i(1, 1): d_pad_frame = 8
 	%DPad.frame = d_pad_frame
+
+func display_stick(stick):
+	var stick_home_pos: Vector2
+	var strength_x = 0.0
+	var strength_y = 0.0
+	match stick.name:
+		"StickLeft":
+			strength_x = Input.get_joy_axis(0, JOY_AXIS_LEFT_X)
+			strength_y = Input.get_joy_axis(0, JOY_AXIS_LEFT_Y)
+			stick_home_pos = stick_left_home_pos
+		"StickRight":
+			strength_x = Input.get_joy_axis(0, JOY_AXIS_RIGHT_X)
+			strength_y = Input.get_joy_axis(0, JOY_AXIS_RIGHT_Y)
+			stick_home_pos = stick_right_home_pos
+
+	var dir = Vector2(strength_x, strength_y)
+	var deg = rad_to_deg(fposmod(dir.rotated(0.5 * PI).angle(), 2 * PI))
+
+	if dir.length() >= 0.95:
+		var stick_frame = 0
+		if (deg >= 345 && deg <= 360) || (deg >= 0 && deg < 15):
+			stick_frame = 1
+		elif deg >= 15 && deg < 45:
+			stick_frame = 2
+		elif deg >= 45 && deg < 75:
+			stick_frame = 3
+		elif deg >= 75 && deg < 105:
+			stick_frame = 4
+		elif deg >= 105 && deg < 135:
+			stick_frame = 5
+		elif deg >= 135 && deg < 165:
+			stick_frame = 6
+		elif deg >= 165 && deg < 195:
+			stick_frame = 7
+		elif deg >= 195 && deg < 225:
+			stick_frame = 8
+		elif deg >= 225 && deg < 255:
+			stick_frame = 9
+		elif deg >= 255 && deg < 285:
+			stick_frame = 10
+		elif deg >= 285 && deg < 315:
+			stick_frame = 11
+		elif (deg >= 315 && deg < 345):
+			stick_frame = 12
+		stick.frame = stick_frame
+		stick.position = stick_home_pos
+	else:
+		stick.frame = 0
+		stick.position = stick_home_pos + Vector2(strength_x * 8, strength_y * 8)
