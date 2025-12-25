aa30b9ed6e05bd5e89213a3b902444b1e4a71c5f
neropatti <28650266+neropatti@users.noreply.github.com>
2025-12-25 22:09:56 +0200

    Remove unused script

--- src/render_align/smoother.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@export var follower: Node2D
-
-func _process(delta: float) -> void:
-	follower.global_position = exp_decay(follower.global_position,self.global_position, 5, delta)
-
-func exp_decay(a, b, decay, delta):
-	return b+(a-b)*exp(-decay*delta)
