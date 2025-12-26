623faaf91261f19838a36b00e2426dba8979725e
AM-Mikey <76826262+AM-Mikey@users.noreply.github.com>
2025-12-26 14:57:16 -0600

    minor fixes
    fixed background clickability in editor, added test background

--- src/RenderAlign/RenderAlign.gd
+++ src/RenderAlign/RenderAlign.gd
@@ -2,14 +2,12 @@ extends Control
 
 @onready var sub_viewport: SubViewport = %SubViewport
 @onready var camera: Camera2D = %Camera2D
-@onready var base_position: Node2D = %"base position"
+@onready var base_position: Node2D = %BasePosition
 
-func _viewport_size_changed():
-	sub_viewport.size = get_viewport().size + Vector2i(20,20)
 
 func _ready() -> void:
-	var _err = get_tree().root.connect("size_changed", Callable(self, "_viewport_size_changed"))
-	_viewport_size_changed()
+	vs.connect("scale_changed", Callable(self, "_resolution_scale_changed"))
+	_resolution_scale_changed(vs.resolution_scale)
 	sub_viewport.world_2d = get_world_2d()
 	get_viewport().canvas_cull_mask = 1
 	sub_viewport.canvas_cull_mask = 2
@@ -18,3 +16,10 @@ func _physics_process(_delta: float) -> void:
 	var active_cam := get_viewport().get_camera_2d()
 	base_position.global_position = active_cam.get_screen_center_position()
 	camera.zoom = active_cam.zoom
+
+
+
+### SIGNALS ###
+
+func _resolution_scale_changed(_resolution_scale):
+	sub_viewport.size = get_viewport().size + Vector2i(20,20)
