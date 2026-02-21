5344670ce0040986818c86f42be1fdcd6874a4b7
neropatti <28650266+neropatti@users.noreply.github.com>
2026-02-21 05:04:47 +0200

    Implement modulate pattern and clean up connection pattern code

--- src/Tile/Wastes.gd
+++ src/Tile/Wastes.gd
@@ -20,41 +20,35 @@ var active_tile_map_cells = []
 	#do_auto_tile()
 
 
-func do_auto_tile(start_coords, start_layer):
+func do_auto_tile(start_coords: Vector2i, start_layer):
 	var data = tile_map.get_child(start_layer).get_cell_tile_data(start_coords)
 	var auto_tile_group = data.get_custom_data("auto_tile_group")
-	if auto_tile_group != "":
-		if auto_tile_group == "backdirt" or auto_tile_group == "ground":
-			var group = auto_tile_group
 
-			var connected_cells = find_all_connected_cells(start_coords, start_layer, group)
-			print(connected_cells)
-			active_tile_map_cells.append(start_coords)
-			if !connected_cells.is_empty():
-				find_cells_recursive(connected_cells, start_layer, group)
+	var auto_tile_data = get_auto_tile_data(auto_tile_group)
 
-			if data.get_custom_data("dont_replace") == true:
-				active_tile_map_cells.erase(start_coords)
+	if auto_tile_data["type"] == "connections":
+		var connected_cells = find_all_connected_cells(start_coords, start_layer, auto_tile_group)
+		active_tile_map_cells.append(start_coords)
+		active_tile_map_cells.append_array(connected_cells)
 
-			for i in active_tile_map_cells:
-				pattern(start_layer, i, group)
-			#print("a: ", active_tile_map_cells)
-			active_tile_map_cells = []
-		else:
-			get_auto_tile_directions(auto_tile_group)
+		if data.get_custom_data("auto_tile_dont_replace") == true:
+			active_tile_map_cells.erase(start_coords)
 
+		for i in active_tile_map_cells:
+			pattern(start_layer, i, auto_tile_group)
+	elif auto_tile_data["type"] == "modulate":
+		var mod_size: Vector2i = auto_tile_data["mod_size"]
+		var mod_origin: Vector2i = auto_tile_data["mod_origin"]
+		var tile_x := start_coords.x % mod_size.x + mod_origin.x
+		var tile_y := start_coords.y % mod_size.y + mod_origin.y
+		var new_atlas_coords := Vector2i(tile_x, tile_y)
+		var tile_map_layer: TileMapLayer = tile_map.get_child(start_layer)
+		tile_map_layer.set_cell(start_coords, 0, new_atlas_coords)
 
-func find_cells_recursive(coords_array, layer, group):
-	for i in coords_array:
-		if !active_tile_map_cells.has(i):
-			var connected_cells = find_all_connected_cells(i, layer, group)
-			active_tile_map_cells.append(i)
-			if !connected_cells.is_empty():
-				find_cells_recursive(connected_cells, layer, group)
+	active_tile_map_cells = []
 
-
-func find_all_connected_cells(coords, layer, group) -> Array:
-	var out = []
+func find_all_connected_cells(coords, layer, group) -> Array [Vector2i]:
+	var out: Array [Vector2i] = []
 	var neighbors = [
 		coords + NW,
 		coords + N,
@@ -70,16 +64,13 @@ func find_all_connected_cells(coords, layer, group) -> Array:
 		if tile_map_layer.get_cell_atlas_coords(neighbor) != Vector2i(-1, -1): #cell is filled
 			var neighbor_data = tile_map_layer.get_cell_tile_data(neighbor)
 			if neighbor_data.get_custom_data("auto_tile_group") == group:
-				if neighbor_data.get_custom_data("dont_replace") == true:
+				if neighbor_data.get_custom_data("auto_tile_dont_replace") == true:
 					continue
 				out.append(neighbor)
 	return out
 
-
-
-
 func pattern(layer, coords, pattern: String):
-	var dict = get_auto_tile_directions(pattern)
+	var dict = get_auto_tile_data(pattern)
 	var group = dict["all"]
 
 	var non_connections = []
@@ -97,9 +88,6 @@ func pattern(layer, coords, pattern: String):
 	match non_connections:
 		[N,S,W,E]:
 			pass
-			#print("single")
-			# TODO: Make this handle empty connection groups..?
-			# Or then just require the tileset editor to put "north" as also "north-west" and "north-east"...
 		[N,W]:
 			new_atlas_coords = dict["nw"].pick_random()
 		[N,E]:
@@ -125,12 +113,14 @@ func pattern(layer, coords, pattern: String):
 
 ### HELPERS ###
 
-func get_auto_tile_directions(auto_tile_group) -> Dictionary:
+func get_auto_tile_data(auto_tile_group) -> Dictionary:
 	var out = {}
 	out["all"] = [] #all tiles in group
 	var source = tile_map.get_child(0).tile_set.get_source(0)
 
 	# TODO: For modulates, find the rect size and do the shit lol
+	var is_modulate_pattern: bool = false
+	var is_connection_pattern: bool = false
 
 	for x in source.texture_region_size.x:
 		for y in source.texture_region_size.y:
@@ -144,10 +134,38 @@ func get_auto_tile_directions(auto_tile_group) -> Dictionary:
 							var auto_tile_directions: String = tile_data.get_custom_data("auto_tile_direction")
 							var directions = auto_tile_directions.split(",")
 							for auto_tile_direction in directions:
+								if auto_tile_direction == "m":
+									is_modulate_pattern = true
+								else:
+									is_connection_pattern = true
 								if out.has(auto_tile_direction):
 									out[auto_tile_direction].append(coords)
 								else:
 									out[auto_tile_direction] = [coords]
+	if is_modulate_pattern and is_connection_pattern:
+		printerr("Auto-tile pattern %s contains both connection and modulate tiles!" % auto_tile_group)
+
+	if is_modulate_pattern == true:
+		out["type"] = "modulate"
+
+	if is_connection_pattern == true:
+		out["type"] = "connections"
+
+	if is_modulate_pattern == true:
+		var min_x: int = out["m"][0].x
+		var max_x: int = min_x
+		var min_y: int = out["m"][0].y
+		var max_y: int = min_y
+		for tile_atlas_pos: Vector2i in out["m"]:
+			min_x = mini(tile_atlas_pos.x, min_x)
+			max_x = maxi(tile_atlas_pos.x, max_x)
+			min_y = mini(tile_atlas_pos.y, min_y)
+			max_y = maxi(tile_atlas_pos.y, max_y)
+		var size: Vector2i = Vector2i(max_x - min_x, max_y - min_y) + Vector2i(1, 1)
+		print("Mod pattern size: ", size)
+		out["mod_size"] = size
+		out["mod_origin"] = Vector2i(min_x, min_y)
+
 	return out
 
 
