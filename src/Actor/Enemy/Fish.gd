986b1208427b03d46a3a8775a158deda05baa917
SanpoNeko <sanponeko23@gmail.com>
2026-03-10 15:16:03 +0700

    Bonk

--- src/Actor/Enemy/Fish.gd
+++ src/Actor/Enemy/Fish.gd
@@ -16,6 +16,8 @@ var jump_pos: Vector2 = global_position
 @onready var ap: AnimationPlayer = get_node("AnimationPlayer")
 @onready var rc: RayCast2D = get_node("RayCast2D")
 
+@onready var BONK: PackedScene = preload("res://src/Effect/BonkParticle.tscn")
+var did_bonk: bool = false
 
 func setup():
 	change_state("idle")
@@ -44,6 +46,19 @@ func on_x_max_changed(new):
 	x_max = new
 	update_path_lines()
 
+func bonk():
+	if (did_bonk or get_slide_collision_count() == 0):
+		return
+
+	var slide_collision: KinematicCollision2D = get_last_slide_collision()
+	var bonk_effect = BONK.instantiate()
+	#print("Fish bonked ", slide_collision.get_normal())
+	bonk_effect.normal = slide_collision.get_normal()
+	bonk_effect.global_position.x = global_position.x
+	bonk_effect.global_position.y = global_position.y + 7.0
+	w.get_node("Front").add_child(bonk_effect)
+	did_bonk = true
+
 func calc_velocity(move_dir, _do_gravity = true, _do_acceleration = true, _do_friction = true) -> Vector2:
 	match state:
 		"idle", "swim":
@@ -58,12 +73,16 @@ func calc_velocity(move_dir, _do_gravity = true, _do_acceleration = true, _do_fr
 			out.y += gravity * get_physics_process_delta_time()
 			if move_dir.y < 0:
 				out.y = speed.y * move_dir.y
+			elif is_on_ceiling() and !did_bonk:
+				out.y = -10
 
 			return out
 		"fall":
 			var out = velocity
-
-			out.y += gravity * get_physics_process_delta_time()
+			if (is_on_ceiling() and !did_bonk):
+				out.y = -10
+			else:
+				out.y += gravity * get_physics_process_delta_time()
 
 			return out
 	return Vector2.ZERO
@@ -72,6 +91,7 @@ func calc_velocity(move_dir, _do_gravity = true, _do_acceleration = true, _do_fr
 #region STATES
 #region Idle
 func enter_idle(_prev_state: String):
+	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
 	if can_move_x:
 		change_state("swim")
 	else:
@@ -85,7 +105,7 @@ func do_idle():
 	var collision = get_node("RayCast2D").get_collider()
 	if collision != null:
 		if (collision is not TileMapLayer and collision.get_collision_layer_value(1)):
-			if (abs(collision.global_position.x - global_position.x) <= 1):
+			if (abs(collision.global_position.x - rc.global_position.x) <= 1):
 				if (debug): print("got target")
 				change_state("attack")
 				return
@@ -93,6 +113,7 @@ func do_idle():
 
 #region Swim
 func enter_swim(_prev_state: String, _arg: Dictionary = {}):
+	motion_mode = CharacterBody2D.MOTION_MODE_FLOATING
 	move_dir.x = swim_dir_x
 	ap.play("Idle")
 
@@ -104,12 +125,12 @@ func do_swim():
 	var collision = rc.get_collider()
 	if collision != null:
 		if (collision is not TileMapLayer and collision.get_collision_layer_value(1)):
-			if (abs(collision.global_position.x - global_position.x) <= 1): #Prevent the fish target just the side of player hitbox
+			if (abs(collision.global_position.x - rc.global_position.x) <= 1): #Prevent the fish target just the side of player hitbox
 				if debug: print("got target")
 				change_state("attack")
 				return
 
-	if is_on_wall():
+	if is_on_wall() and (move_and_collide(Vector2.RIGHT, true) or move_and_collide(Vector2.LEFT, true)):
 		swim_dir_x = -swim_dir_x
 	else:
 		if global_position.x < start_pos.x + x_min * 16:
@@ -123,14 +144,18 @@ func do_swim():
 
 #region Attack
 func enter_attack(_prev_state: String):
+	motion_mode = CharacterBody2D.MOTION_MODE_GROUNDED
 	jump_pos = global_position
 	ap.play("Target")
+	did_bonk = false
 
 	await ap.animation_finished
 	move_dir = Vector2.UP
 
 func do_attack():
 	if position.y <= jump_pos.y - jump_height * 16 + speed.y * 0.3 or is_on_ceiling():
+		if (is_on_ceiling()):
+			bonk()
 		change_state("fall")
 		return
 
@@ -139,13 +164,20 @@ func do_attack():
 #endregion
 
 #region Fall
-func enter_fall(_prev_state: String):
-	ap.play("Fall")
-
 func exit_fall(_next_state: String):
 	velocity = Vector2.ZERO
 
 func do_fall():
+	if (velocity.y > 0):
+		if (ap.current_animation != "Fall"):
+			ap.play("Fall")
+	else:
+		if (ap.current_animation != "Rise"):
+			ap.play("Rise")
+
+	if (is_on_ceiling()):
+		bonk()
+
 	if !(global_position.y >= jump_pos.y - 5 or is_on_floor()):
 		velocity = calc_velocity(move_dir)
 		move_and_slide()
