458be147f69cb709dcb18be02118865170fdb67f
AM-Mikey <76826262+AM-Mikey@users.noreply.github.com>
2026-04-07 23:31:36 -0500

    started fixing tongue code

--- src/Actor/Enemy/Frog.gd
+++ src/Actor/Enemy/Frog.gd
@@ -5,46 +5,49 @@ const TX_0 = preload("res://assets/Actor/Enemy/Frog.png")
 const TX_1 = preload("res://assets/Actor/Enemy/Frog1.png")
 
 var target
-var see_target: bool = false
 
 @export var jump_delay = 3.0
 @export var difficulty := 1
 var max_difficulty := 1
 @export var croak_time = 1.0
 var move_dir = Vector2.ZERO
-var look_dir = Vector2.LEFT:
-	set(val):
-		$TongueDetection.target_position.x = (8.0 + tongue_range) * val.x
-		look_dir = val
+var look_dir = Vector2.LEFT
 
-@export var tongue_damage: float = 2.0
-var tongue_range: float = 50.0:
-	set(val):
-		$TongueDetection.target_position.x = (8.0 + val) * look_dir.x
-		tongue_range = val
+@export var tongue_damage := 2.0
+var tongue_length := 0.0
+var tongue_max_length : float
 var tongue_speed := 150.0
-var tongue_tween: Tween
+var tongue_ready_time := 0.5
+var tongue_unready_time := 2.0
+var tongue_cooldown_time := 6.0
 
 func setup():
 	change_state("idle")
 	look_dir = $LookVector.direction
-	tongue_range = abs($TongueRange.position.x)
+	tongue_max_length = abs($TongueRange.position.x)
+	$TonguePlayerCast.target_position = Vector2($TongueRange.position.x, 0.0)
+	$Tongue/WorldCast.target_position = Vector2($TongueRange.position.x, 0.0)
+	var rect = RectangleShape2D.new()
+	rect.size = Vector2(tongue_max_length, 2)
+	$Tongue/CollisionShape2D.shape = rect
+	$Tongue/CollisionShape2D.position = Vector2(look_dir.x * -1 * (tongue_max_length / 2.0), -7)
+
 	set_floor_stop_on_slope_enabled(true)
 	match difficulty:
 		0:
-			$TongueDetection.enabled = false
+			$TonguePlayerCast.enabled = false
 			hp = 3
 			reward = 2
 			damage_on_contact = 1
 			$Sprite2D.texture = TX_0
-			speed = Vector2(60, 120)
+			speed = Vector2(60, 130)
 		1:
-			$TongueDetection.enabled = true
+			$TonguePlayerCast.enabled = true
 			hp = 4
 			reward = 3
 			damage_on_contact = 2
 			$Sprite2D.texture = TX_1
-			speed = Vector2(12, 120)
+			speed = Vector2(35, 100)
 
 
 func _on_physics_process(_delta):
@@ -56,35 +59,33 @@ func _on_physics_process(_delta):
 	move_and_slide()
 	animate()
 
-	if (target):
-		see_target = check_to_see_player(target)
 
 
 
 ### STATES ###
 
-func do_idle(_delta):
-	if difficulty == 1 && is_on_floor() && tongue_detection_see_player() && $TongueTimer.is_stopped():
-		change_state("tongue_attack")
-		return
-	elif (see_target):
-		change_state("targeting")
+func do_idle(_delta): #might add tongue ready here as well
+	#if difficulty == 1:
+		#pass
+	if target:
+		change_state("jump_ready")
 		return
 
-func do_targeting(_delta):
+func do_jump_ready(_delta):
 	if is_on_floor():
-		if difficulty == 1 && tongue_detection_see_player() && $TongueTimer.is_stopped():
-			change_state("tongue_attack")
-			return
-		elif $JumpTimer.time_left == 0.0:
+		if $JumpTimer.is_stopped():
 			change_state("jump")
-			return
-		elif !see_target:
-			change_state("idle")
 			return
 		elif target:
 			look_dir = Vector2(sign(target.global_position.x - global_position.x), 0)
-
+			$Tongue.scale.x = look_dir.x * -1
+			$TonguePlayerCast.scale.x = look_dir.x * -1
+			if tongue_player_cast_check() && $TongueCooldown.is_stopped():
+				change_state("tongue_ready")
+				return
+		elif target == null:
+			change_state("idle")
+			return
 
 
 func enter_jump(_last_state):
@@ -99,67 +100,61 @@ func do_jump(_delta):
 		$AnimationPlayer.play("Stand")
 		$JumpTimer.start(jump_delay)
 		$CroakTimer.start(croak_time)
-		if see_target:
-			change_state("targeting")
+		if target:
+			change_state("jump_ready")
 			return
 		else:
 			change_state("idle")
 			return
 
+func enter_tongue_ready(_prev_state):
+	$AnimationPlayer.play("TongueReady")
+	$TongueReadyTimer.start(tongue_ready_time)
+	$TongueUnreadyTimer.start(tongue_unready_time)
 
+func do_tongue_ready(_delta):
+	if is_on_floor() && tongue_player_cast_check() && $TongueReadyTimer.is_stopped():
+		change_state("tongue_out")
+		return
+	if $TongueUnreadyTimer.is_stopped():
+		change_state("tongue_unready")
+		return
 
-func enter_tongue_attack(_prev_state):
-	var tongue_time: float = tongue_range / tongue_speed
-	$Tongue/CollisionShape2D.shape.size = Vector2(8.0, 4.0)
-	$Tongue/CollisionShape2D.position = Vector2(4.0, -4.0) * Vector2(look_dir.x, 1.0)
-	$Tongue/Sprite.visible = true
-	move_dir = Vector2.ZERO
-	$Tongue/CollisionShape2D.disabled = false
-	$Tongue/WorldCast.target_position.x = 8.0 * look_dir.x
-	$Tongue/WorldCast.enabled = true
-	tongue_tween = create_tween()
-	tongue_tween.set_parallel()
-	tongue_tween.tween_property($Tongue/CollisionShape2D.shape, "size", Vector2(8.0 + tongue_range, 4.0), tongue_time)
-	tongue_tween.tween_property($Tongue/CollisionShape2D, "position", Vector2(4.0 + tongue_range / 2.0, -4.0) * Vector2(look_dir.x, 1.0), tongue_time)
-	tongue_tween.tween_property($Tongue/WorldCast, "target_position:x", (8.0 + tongue_range) * look_dir.x, tongue_time)
+func enter_tongue_unready(_prev_state):
+	$AnimationPlayer.play("TongueUnready")
+	await $AnimationPlayer.animation_finished
+	change_state("idle") #might go to another state if neccesary
+	return
 
-func do_tongue_attack(_delta):
-	# Later on when there's proper sprite, remove all thing related to $Tongue/Sprite
-	$Tongue/Sprite.polygon = from_rectangle_to_polygon(Rect2($Tongue/CollisionShape2D.position - $Tongue/CollisionShape2D.shape.size / 2.0, $Tongue/CollisionShape2D.shape.size))
-
-	if tongue_tween:
-		if !tongue_tween.is_running() || $Tongue/WorldCast.is_colliding():
-			change_state("tongue_retract")
-			return
-
-func exit_tongue_attack(_next_state):
-	if (tongue_tween):
-		tongue_tween.kill()
-		tongue_tween = null
-
-
-
-func enter_tongue_retract(_prev_state):
-	var tongue_time: float = abs($Tongue/WorldCast.target_position.x - (8.0 * look_dir.x)) / tongue_speed
+func enter_tongue_out(_prev_state): #TODO: change collision shape scale/position as well, add some effect when lick is successful?
+	$TongueCooldown.start(tongue_cooldown_time)
+	$AnimationPlayer.play("TongueOut")
+	$Tongue/Sprite2D.frame = difficulty
+	await $AnimationPlayer.animation_finished
 	var tween = create_tween()
-	tween.set_parallel()
-	tween.tween_property($Tongue/CollisionShape2D.shape, "size", Vector2(8.0, 4.0), tongue_time)
-	tween.tween_property($Tongue/CollisionShape2D, "position", Vector2(4.0, -4.0) * Vector2(look_dir.x, 1.0), tongue_time)
-	tween.tween_property($Tongue/WorldCast, "target_position:x", 8.0 * look_dir.x, tongue_time)
+	var tongue_duration = tongue_max_length / tongue_speed
+	tongue_length = 16 #to start with
+	tween.tween_property(self, "tongue_length", tongue_max_length, tongue_duration)
+
+func do_tongue_out(_delta):
+	var tongue_scale = ((tongue_length - 16)/ 16.0) #16 for the start distance
+	$Tongue/Sprite2D.scale.x = max(tongue_scale, 0.0) * -1
+	if tongue_length == tongue_max_length:
+		change_state("tongue_in")
+
+func enter_tongue_in(_prev_state):
+	var tween = create_tween()
+	var tongue_duration = tongue_max_length / tongue_speed
+	tween.tween_property(self, "tongue_length", 16.0, tongue_duration)
 	await tween.finished
-	$Tongue/WorldCast.enabled = false
-	$Tongue/CollisionShape2D.disabled = true
-	$Tongue/Sprite.visible = false
-	$TongueTimer.start()
-	if (see_target):
-		change_state("targeting")
-	else:
-		change_state("idle")
+	$AnimationPlayer.play("TongueIn")
+	await $AnimationPlayer.animation_finished
+	change_state("idle") #might go to another state if neccesary
+	return
 
-func do_tongue_attack_retract(_delta):
-	# Later on when there's proper sprite, remove this
-	$Tongue/Sprite.polygon = from_rectangle_to_polygon(Rect2($Tongue/CollisionShape2D.position - $Tongue/CollisionShape2D.shape.size / 2.0, $Tongue/CollisionShape2D.shape.size))
-
+func do_tongue_in(_delta):
+	var tongue_scale = ((tongue_length - 16)/ 16.0) #16 for the start distance
+	$Tongue/Sprite2D.scale.x = max(tongue_scale, 0.0) * -1
 
 
 ### EFFECT ###
@@ -182,31 +177,16 @@ func animate():
 
 ### HELPER ###
 
-func check_to_see_player(player) -> bool:
-	var raycast_parameter: PhysicsRayQueryParameters2D = PhysicsRayQueryParameters2D.new()
-	raycast_parameter.from = global_position + Vector2(0.0, -14.0)
-	raycast_parameter.to = player.global_position
-	raycast_parameter.collide_with_areas = false
-	raycast_parameter.collide_with_bodies = true
-	raycast_parameter.collision_mask = 8 + 256 # world and breakable block
-	var raycast_result: Dictionary = get_world_2d().direct_space_state.intersect_ray(raycast_parameter)
-	if !(raycast_result.is_empty()):
+func tongue_player_cast_check() -> bool:
+	if !($TonguePlayerCast.is_colliding()):
 		return false
-	return true
-
-func tongue_detection_see_player() -> bool:
-	if !($TongueDetection.is_colliding()):
-		return false
-	var collider = $TongueDetection.get_collider()
+	var collider = $TonguePlayerCast.get_collider()
 	if (collider):
 		if (collider is not TileMapLayer):
 			if (collider.get_collision_layer_value(1)):
 				return true
 	return false
 
-func from_rectangle_to_polygon(rect: Rect2) -> PackedVector2Array:
-	var res: PackedVector2Array = [rect.position, rect.position + Vector2(rect.size.x, 0.0), rect.position + rect.size, rect.position + Vector2(0.0, rect.size.y)]
-	return res
 
 
 
@@ -217,10 +197,9 @@ func _on_PlayerDetector_body_entered(body):
 
 func _on_PlayerDetector_body_exited(_body):
 	target = null
-	see_target = false
 
 func _on_croak_timer_timeout():
-	if state == "targeting":
+	if state == "jump_ready":
 		$AnimationPlayer.play("Croak")
 
 func _on_Tongue_area_entered(area: Area2D) -> void:
