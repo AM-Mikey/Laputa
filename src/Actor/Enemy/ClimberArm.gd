0da7e94b871994cb3159a54da227bbee34acba32
AM-Mikey <76826262+AM-Mikey@users.noreply.github.com>
2026-04-15 21:00:25 -0500

    started updating climber sprite rotation

--- src/Actor/Enemy/ClimberArm.gd
+++ src/Actor/Enemy/ClimberArm.gd
@@ -10,5 +10,11 @@ func setup():
 	damage_on_contact = 1
 	$Label.visible = get_parent().get_parent().debug
 
+func _on_physics_process(_delta):
+	var parent_direction = global_position - get_parent().global_position
+	var angle_to_parent = parent_direction.angle()
+	var frame_index = posmod(round(angle_to_parent / (TAU / 8)), 8)
+	$Sprite2D.frame_coords.x = frame_index
+
 func do_death_routine():
 	arm_die.emit(self)
