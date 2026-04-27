94db1a66b7a5fc87048325dac0fe64e127cb3519
SanpoNeko <sanponeko23@gmail.com>
2026-04-27 16:44:42 +0700

    + Add vertical spawn + Add infer spawning

--- src/Trigger/ScreenSpawner.gd
+++ src/Trigger/ScreenSpawner.gd
@@ -12,9 +12,10 @@ extends Trigger
 ## The spawner will infer the direction from how the player enters the spawn zone.
 @export var spawn_horizontal: bool = true
 @export var spawn_vertical: bool = true
+@export var spawn_on_opposite_screen_edge: bool = true
 ## If set to any value different front Vector2.ZERO, spawn_horizontal, spawn_vertical will be ignored and spawn direction will always set to this value.
 @export var only_spawn_direction: Vector2 = Vector2.ZERO
-## If spawn in horizontal directions, the spawner will only take the top and bottom edges as limit.
+## If spawn in horizontal directions, the spawner will only take the top and bottom edges as limits.
 ## Similar for vertical direction, the spawner will only take the left and right edges as limits
 var spawn_area: Rect2
 
@@ -48,7 +49,7 @@ var curr_spawn_direction := Vector2.ZERO:
 
 func _ready():
 	trigger_type = "screen_spawner"
-	spawn_area = Rect2( - $CollisionShape2D.shape.size / 2.0, $CollisionShape2D.shape.size)
+	spawn_area = Rect2(-$CollisionShape2D.shape.size / 2.0, $CollisionShape2D.shape.size)
 	$SpawnTimer.wait_time = spawn_interval
 	spawn_area = $SpawnArea.value
 
@@ -103,104 +104,128 @@ func _on_SpawnTimer_timeout() -> void:
 	var enemy_distance_on_ortho: float = 0.0
 
 	# Remove all invalid processed enemy. Including dead
-	var to_be_deleted_enemy: Array = processed_enemy.filter(func (ele): return  !ele or !is_instance_valid(ele) or ele.is_queued_for_deletion())
+	var to_be_deleted_enemy: Array = processed_enemy.filter(func (ele): return !ele or !is_instance_valid(ele) or ele.is_queued_for_deletion())
 	for en in to_be_deleted_enemy:
 		if is_instance_valid(en):
 			en.queue_free()
 
-	# WIP full screen spawn
-	match curr_spawn_direction:
-		Vector2.RIGHT, Vector2.LEFT:
-			# Find the farthest spawned one on the screen. If there is no enemy or the farthest one is less than the screen edge, default to that screen edge
-			var default_spawn_screen_edge_x: float = screen_position.x if curr_spawn_direction == Vector2.RIGHT else screen_position.x + screen_size.x
-			var curr_x: float = default_spawn_screen_edge_x
-			var valid_processed_enemy: Array = processed_enemy.filter(func (ele): return ele not in to_be_deleted_enemy)
-			if valid_processed_enemy.size() > 0:
+	# Find the farthest spawned one on the screen. If there is no enemy or the farthest one is less than the screen edge, default to that screen edge
+	var default_spawn_screen_edge: float = get_screen_edge_position()
+	var curr_pos: float = default_spawn_screen_edge
+	var valid_processed_enemy: Array = processed_enemy.filter(func (ele): return ele not in to_be_deleted_enemy)
+	if valid_processed_enemy.size() > 0:
+		match curr_spawn_direction:
+			Vector2.LEFT, Vector2.RIGHT:
 				var farthest_spawned_x: float = 0.0
-				if curr_spawn_direction == Vector2.RIGHT:
-					farthest_spawned_x = valid_processed_enemy.reduce(func (accum, ele): return min(ele.global_position.x, accum), 9999999999)
-				else:
+				if (curr_spawn_direction == Vector2.LEFT):
 					farthest_spawned_x = valid_processed_enemy.reduce(func (accum, ele): return max(ele.global_position.x, accum), -9999999999)
-				if (curr_spawn_direction == Vector2.RIGHT and farthest_spawned_x < default_spawn_screen_edge_x) or \
-					(curr_spawn_direction == Vector2.LEFT and farthest_spawned_x > default_spawn_screen_edge_x):
-					curr_x = farthest_spawned_x + enemy_distance * -curr_spawn_direction.x
-			processed_enemy = valid_processed_enemy
+				else:
+					farthest_spawned_x = valid_processed_enemy.reduce(func (accum, ele): return min(ele.global_position.x, accum), 9999999999)
+				if (curr_spawn_direction == Vector2.RIGHT and farthest_spawned_x < default_spawn_screen_edge) or \
+				(curr_spawn_direction == Vector2.LEFT and farthest_spawned_x > default_spawn_screen_edge):
+					curr_pos = farthest_spawned_x + enemy_distance * -curr_spawn_direction.x
+			Vector2.UP, Vector2.DOWN:
+				var farthest_spawned_y: float = 0.0
+				if (curr_spawn_direction == Vector2.UP):
+					farthest_spawned_y = valid_processed_enemy.reduce(func (accum, ele): return max(ele.global_position.y, accum), -9999999999)
+				else:
+					farthest_spawned_y = valid_processed_enemy.reduce(func (accum, ele): return min(ele.global_position.y, accum), 9999999999)
+				if (curr_spawn_direction == Vector2.UP and farthest_spawned_y > default_spawn_screen_edge) or \
+				(curr_spawn_direction == Vector2.DOWN and farthest_spawned_y < default_spawn_screen_edge):
+					curr_pos = farthest_spawned_y + enemy_distance * -curr_spawn_direction.y
 
-			#region Test 1: Spawn an enemy at the edge and many more standby (disabled and invis until on screen) further out from the edge
-			## Resulting in some to spawn fast or slow depending on the screen move speed
-			## Fresh spawn from the edge of the screen
-			#var final_x: float = curr_x + 1000.0 * -curr_spawn_direction.x
-			#var enemy = enemy_scene.instantiate()
-			#enemy.dir = curr_spawn_direction
-			#enemy.global_position.y = randf() * screen_size.y + screen_position.y
-			#enemy.global_position.x = curr_x
-			#actors.add_child(enemy)
-			#processed_enemy.append(enemy)
-			#curr_x += enemy_distance * -curr_spawn_direction.x
+	processed_enemy = valid_processed_enemy
+
+	#region Test 1: Spawn an enemy at the edge and many more standby (disabled and invis until on screen) further out from the edge
+	## Resulting in some to spawn fast or slow depending on the screen move speed
+	## Fresh spawn from the edge of the screen
+	#var final_x: float = curr_pos + 1000.0 * -curr_spawn_direction.x
+	#var enemy = enemy_scene.instantiate()
+	#enemy.dir = curr_spawn_direction
+	#enemy.global_position.y = randf() * screen_size.y + screen_position.y
+	#enemy.global_position.x = curr_pos
+	#actors.add_child(enemy)
+	#processed_enemy.append(enemy)
+	#curr_pos += enemy_distance * -curr_spawn_direction.x
 #
-			## Spawn aheads but will not be active until enter screen
-			#while !is_equal_approx(curr_x, final_x):
-				#var visible_notifier: VisibleOnScreenEnabler2D = VisibleOnScreenEnabler2D.new()
-				#var enemy_ahead = enemy_scene.instantiate()
-				#enemy_ahead.process_mode = PROCESS_MODE_DISABLED
-				#enemy_ahead.visible = false
-				#enemy_ahead.global_position.y = randf() * screen_size.y + screen_position.y
-				#enemy_ahead.global_position.x = curr_x
-				#enemy_ahead.dir = curr_spawn_direction
-				#var enemy_ahead_boundary_rect = enemy_ahead.get_node("CollisionShape2D").shape.get_rect()
-				#visible_notifier.enable_mode = VisibleOnScreenEnabler2D.ENABLE_MODE_INHERIT
-				#visible_notifier.rect = enemy_ahead_boundary_rect
-				#visible_notifier.show_rect = true # Later change
-				#visible_notifier.screen_entered.connect(visible_notifier.queue_free)
-				#visible_notifier.screen_entered.connect(unprocessed_enemy.erase.bind(enemy_ahead))
-				#visible_notifier.screen_entered.connect(processed_enemy.append.bind(enemy_ahead))
+	## Spawn aheads but will not be active until enter screen
+	#while !is_equal_approx(curr_pos, final_x):
+		#var visible_notifier: VisibleOnScreenEnabler2D = VisibleOnScreenEnabler2D.new()
+		#var enemy_ahead = enemy_scene.instantiate()
+		#enemy_ahead.process_mode = PROCESS_MODE_DISABLED
+		#enemy_ahead.visible = false
+		#enemy_ahead.global_position.y = randf() * screen_size.y + screen_position.y
+		#enemy_ahead.global_position.x = curr_pos
+		#enemy_ahead.dir = curr_spawn_direction
+		#var enemy_ahead_boundary_rect = enemy_ahead.get_node("CollisionShape2D").shape.get_rect()
+		#visible_notifier.enable_mode = VisibleOnScreenEnabler2D.ENABLE_MODE_INHERIT
+		#visible_notifier.rect = enemy_ahead_boundary_rect
+		#visible_notifier.show_rect = true # Later change
+		#visible_notifier.screen_entered.connect(visible_notifier.queue_free)
+		#visible_notifier.screen_entered.connect(unprocessed_enemy.erase.bind(enemy_ahead))
+		#visible_notifier.screen_entered.connect(processed_enemy.append.bind(enemy_ahead))
 #
-				#enemy_ahead.add_child(visible_notifier)
-				#actors.add_child(enemy_ahead)
-				#unprocessed_enemy.append(enemy_ahead)
-				#curr_x += enemy_distance * -curr_spawn_direction.x
-			#endregion
+		#enemy_ahead.add_child(visible_notifier)
+		#actors.add_child(enemy_ahead)
+		#unprocessed_enemy.append(enemy_ahead)
+		#curr_pos += enemy_distance * -curr_spawn_direction.x
+	#endregion
 
-			#region Test 2: Spawn enemies preemptively and continue spawning from the farthest one
-			enemy_distance_on_ortho = screen_size.y
-			var average_one_wave_spawn_on_ortho: int = ceil(spawn_area.size.y / enemy_distance_on_ortho)
-			var to_spawn: int = 0
-			var enemy_beyond_screen_edge: int = 0
-			if (curr_spawn_direction == Vector2.RIGHT):
-				enemy_beyond_screen_edge = processed_enemy.reduce(func (accum, ele): return accum + 1 if ele.global_position.x < default_spawn_screen_edge_x else accum, 0)
+	#region Test 2: Spawn enemies preemptively and continue spawning from the farthest one
+	enemy_distance_on_ortho = screen_size.y if curr_spawn_direction in [Vector2.LEFT, Vector2.RIGHT] else screen_size.x
+	var average_one_wave_spawn_on_ortho: int = ceil(spawn_area.size.y / enemy_distance_on_ortho)
+	var to_spawn: int = 0
+	var enemy_beyond_screen_edge: int = 0
+	match curr_spawn_direction:
+		Vector2.RIGHT:
+			enemy_beyond_screen_edge = processed_enemy.reduce(func (accum, ele): return accum + 1 if ele.global_position.x < default_spawn_screen_edge else accum, 0)
+		Vector2.LEFT:
+			enemy_beyond_screen_edge = processed_enemy.reduce(func (accum, ele): return accum + 1 if ele.global_position.x > default_spawn_screen_edge else accum, 0)
+		Vector2.UP:
+			enemy_beyond_screen_edge = processed_enemy.reduce(func (accum, ele): return accum + 1 if ele.global_position.y < default_spawn_screen_edge else accum, 0)
+		Vector2.DOWN:
+			enemy_beyond_screen_edge = processed_enemy.reduce(func (accum, ele): return accum + 1 if ele.global_position.y > default_spawn_screen_edge else accum, 0)
+
+	if (enemy_beyond_screen_edge < 3 * average_one_wave_spawn_on_ortho):
+		to_spawn = 3
+	elif (enemy_beyond_screen_edge < 5 * average_one_wave_spawn_on_ortho):
+		to_spawn = 2
+	elif (enemy_beyond_screen_edge < 10 * average_one_wave_spawn_on_ortho):
+		to_spawn = 1
+	else:
+		to_spawn = 0
+
+	var curr_spawn_area_ortho_position = curr_spawn_area.position.y if curr_spawn_direction in [Vector2.LEFT, Vector2.RIGHT] else curr_spawn_area.position.x
+	var curr_spawn_area_ortho_size = curr_spawn_area.size.y if curr_spawn_direction in [Vector2.LEFT, Vector2.RIGHT] else curr_spawn_area.size.x
+	var spawn_area_ortho_position = spawn_area.position.y if curr_spawn_direction in [Vector2.LEFT, Vector2.RIGHT] else spawn_area.position.x
+	var spawn_area_ortho_size = spawn_area.size.y if curr_spawn_direction in [Vector2.LEFT, Vector2.RIGHT] else spawn_area.size.x
+
+	var near_screen_ortho: float = curr_spawn_area_ortho_position + curr_spawn_area_ortho_size * randf()
+	var enemy_spread_on_ortho_min_section: int = ceil((spawn_area_ortho_position - near_screen_ortho) / enemy_distance_on_ortho)
+	var enemy_spread_on_ortho_max_section: int = floor((spawn_area_ortho_position + spawn_area_ortho_size - near_screen_ortho) / enemy_distance_on_ortho)
+	#print(curr_spawn_area, " ", screen_rect, " ", spawn_area)
+
+	for i in range(0, to_spawn):
+		for j in range(enemy_spread_on_ortho_min_section, enemy_spread_on_ortho_max_section + 1):
+			var curr_ortho_pos: float = near_screen_ortho + j * enemy_distance_on_ortho * (0.9 + 0.2 * randf())
+			if (spawn_area_ortho_position > curr_ortho_pos or spawn_area_ortho_position + spawn_area_ortho_size < curr_ortho_pos):
+				continue
+			var enemy = enemy_scene.instantiate()
+			if (curr_spawn_direction in [Vector2.LEFT, Vector2.RIGHT]):
+				enemy.global_position.y = curr_ortho_pos
+				enemy.global_position.x = curr_pos + (-0.2 + randf() * 0.1) * enemy_distance
 			else:
-				enemy_beyond_screen_edge = processed_enemy.reduce(func (accum, ele): return accum + 1 if ele.global_position.x > default_spawn_screen_edge_x else accum, 0)
-			if (enemy_beyond_screen_edge < 3 * average_one_wave_spawn_on_ortho):
-				to_spawn = 3
-			elif (enemy_beyond_screen_edge < 5 * average_one_wave_spawn_on_ortho):
-				to_spawn = 2
-			elif (enemy_beyond_screen_edge < 10 * average_one_wave_spawn_on_ortho):
-				to_spawn = 1
-			else:
-				to_spawn = 0
+				enemy.global_position.x = curr_ortho_pos
+				enemy.global_position.y = curr_pos + (-0.2 + randf() * 0.1) * enemy_distance
+			enemy.dir = curr_spawn_direction
 
-			var near_screen_y: float = curr_spawn_area.position.y + curr_spawn_area.size.y * randf()
-			var enemy_spread_on_ortho_min_section: int = ceil((spawn_area.position.y - near_screen_y) / enemy_distance_on_ortho)
-			var enemy_spread_on_ortho_max_section: int = floor((spawn_area.position.y + spawn_area.size.y - near_screen_y) / enemy_distance_on_ortho)
-			#print(curr_spawn_area, " ", screen_rect, " ", spawn_area)
+			actors.add_child(enemy)
+			processed_enemy.append(enemy)
+		curr_pos += enemy_distance * -curr_spawn_direction.x
+		if (curr_pos < level_rect.position.x or curr_pos > level_rect.position.x + level_rect.size.x):
+			break
 
-			for i in range(0, to_spawn):
-				for j in range(enemy_spread_on_ortho_min_section, enemy_spread_on_ortho_max_section + 1):
-					var curr_y: float = near_screen_y + j * enemy_distance_on_ortho * (0.9 + 0.2 * randf())
-					if (spawn_area.position.y > curr_y or spawn_area.position.y + spawn_area.size.y < curr_y):
-						continue
-					var enemy = enemy_scene.instantiate()
-					enemy.global_position.y = curr_y
-					enemy.global_position.x = curr_x + (-0.2 + randf() * 0.1) * enemy_distance
-					enemy.dir = curr_spawn_direction
-
-					actors.add_child(enemy)
-					processed_enemy.append(enemy)
-				curr_x += enemy_distance * -curr_spawn_direction.x
-				if (curr_x < level_rect.position.x or curr_x > level_rect.position.x + level_rect.size.x):
-					break
-
-			#region
+	#endregion
 
 ### SIGNAL ###
 func _on_body_entered(body: Node2D):
@@ -213,20 +238,39 @@ func _on_body_entered(body: Node2D):
 	var detection_rect: Rect2 = Rect2($CollisionShape2D.global_position - $CollisionShape2D.shape.size / 2.0 , $CollisionShape2D.shape.size)
 	var player_x_percent: float = (body.global_position.x - detection_rect.position.x) / detection_rect.size.x
 	var player_y_percent: float = (body.global_position.y - detection_rect.position.y) / detection_rect.size.y
+	print(detection_rect, " ", body.global_position)
+	print(player_x_percent, " ", player_y_percent)
 
-	if (only_spawn_direction == Vector2.ZERO): #WIP: Improve this to a rectangle, this is only suited for sawure
-		if (spawn_horizontal):
-			if (player_x_percent < 0.5):
-				curr_spawn_direction = Vector2.LEFT
+	if (only_spawn_direction == Vector2.ZERO):
+		if (spawn_horizontal and !spawn_vertical):
+			if (player_x_percent <= 0.5):
+				curr_spawn_direction = Vector2.LEFT if spawn_on_opposite_screen_edge else Vector2.RIGHT
 			else:
-				curr_spawn_direction = Vector2.RIGHT
-		elif (spawn_vertical):
-			if (player_y_percent < 0.5):
-				curr_spawn_direction = Vector2.UP
+				curr_spawn_direction = Vector2.RIGHT if spawn_on_opposite_screen_edge else Vector2.LEFT
+		elif (spawn_vertical and !spawn_horizontal):
+			if (player_y_percent <= 0.5):
+				curr_spawn_direction = Vector2.UP if spawn_on_opposite_screen_edge else Vector2.DOWN
 			else:
-				curr_spawn_direction = Vector2.DOWN
+				curr_spawn_direction = Vector2.DOWN if spawn_on_opposite_screen_edge else Vector2.UP
+		elif (spawn_vertical and spawn_horizontal):
+			var check_x_left: float = player_y_percent
+			var check_x_right: float = 1.0 - player_y_percent
 
-	elif (only_spawn_direction):
+			if (player_y_percent <= 0.5):
+				if (player_x_percent >= check_x_left and player_x_percent <= check_x_right):
+					curr_spawn_direction = Vector2.UP if spawn_on_opposite_screen_edge else Vector2.DOWN
+				elif player_x_percent < check_x_left:
+					curr_spawn_direction = Vector2.LEFT if spawn_on_opposite_screen_edge else Vector2.RIGHT
+				else:
+					curr_spawn_direction = Vector2.RIGHT if spawn_on_opposite_screen_edge else Vector2.LEFT
+			else:
+				if (player_x_percent >= check_x_right and player_x_percent <= check_x_left):
+					curr_spawn_direction = Vector2.DOWN if spawn_on_opposite_screen_edge else Vector2.UP
+				elif player_x_percent < check_x_right:
+					curr_spawn_direction = Vector2.LEFT if spawn_on_opposite_screen_edge else Vector2.RIGHT
+				else:
+					curr_spawn_direction = Vector2.RIGHT if spawn_on_opposite_screen_edge else Vector2.LEFT
+	else:
 		curr_spawn_direction = only_spawn_direction
 
 	player_in_trigger = true
@@ -241,16 +285,41 @@ func _on_body_exited(_body: Node2D):
 		en.queue_free()
 	unprocessed_enemy = []
 
+	var screen_edge: float = get_screen_edge_position()
+
+	for en in processed_enemy:
+		if !(en):
+			continue
+		match curr_spawn_direction:
+			Vector2.LEFT:
+				if (en.global_position.x > screen_edge):
+					en.queue_free()
+			Vector2.RIGHT:
+				if (en.global_position.x < screen_edge):
+					en.queue_free()
+			Vector2.UP:
+				if (en.global_position.y > screen_edge):
+					en.queue_free()
+			Vector2.DOWN:
+				if (en.global_position.y < screen_edge):
+					en.queue_free()
+	processed_enemy = processed_enemy.filter(func (ele): return ele and !ele.dead and !ele.is_queued_for_deletion())
+	$SpawnTimer.stop()
+
+func get_screen_edge_position() -> float:
 	var camera: Camera2D = get_viewport().get_camera_2d()
 	var screen_size: Vector2 = get_viewport().get_visible_rect().size / vs.resolution_scale
 	var screen_position: Vector2 = camera.get_screen_center_position() - screen_size / 2.0
-	var screen_edge_x: float = screen_position.x if curr_spawn_direction == Vector2.RIGHT else screen_position.x + screen_size.x
-	for en in processed_enemy:
-		if (curr_spawn_direction == Vector2.RIGHT and en.global_position.x < screen_edge_x) or \
-			(curr_spawn_direction == Vector2.LEFT and en.global_position.x > screen_edge_x):
-			en.queue_free()
-	processed_enemy = processed_enemy.filter(func (ele): return !ele and !ele.dead and !ele.is_queued_for_deletion())
-	$SpawnTimer.stop()
+	match curr_spawn_direction:
+		Vector2.LEFT:
+			return screen_position.x + screen_size.x
+		Vector2.RIGHT:
+			return screen_position.x
+		Vector2.UP:
+			return screen_position.y + screen_size.y
+		Vector2.DOWN:
+			return screen_position.y
+	return 0.0
 
 func _on_despawn_boundary_body_entered(actor: Enemy) -> void:
 	if (actor in processed_enemy):
