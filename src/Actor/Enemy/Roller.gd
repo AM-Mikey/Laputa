83b605267d8894bba25bd6d1a31f67a8c8e25ba2
SanpoNeko <sanponeko23@gmail.com>
2026-06-03 10:48:36 +0700

    - Roller: Remove random bouncing when roll on a slope

--- src/Actor/Enemy/Roller.gd
+++ src/Actor/Enemy/Roller.gd
@@ -8,8 +8,12 @@ var move_dir
 
 @onready var prev_global_position := global_position
 var stuck := false
-var stuck_grace_timer := 0.1
+var stuck_grace_timer := 0.2
 var stuck_timer := 0.0
+
+var current_vel := Vector2.ZERO
+var on_floor: bool = false
+var on_slope: bool = false
 
 func setup(): #Reminder: no function called can use await
 	hp = 3
@@ -29,12 +33,17 @@ func setup(): #Reminder: no function called can use await
 
 func _on_physics_process(delta):
 	if disabled or dead: return
-	velocity = calc_velocity(move_dir, true, false, false)
+	on_floor = $Floor.is_colliding()
+	on_slope = (move_dir.x > 0 and !$LFloor.is_colliding() and $RFloor.is_colliding()) \
+				or (move_dir.x <= 0 and !$RFloor.is_colliding() and $LFloor.is_colliding())
 
-	if is_on_floor(): # Avoid getting stuck on weird geometry
-		velocity.y = 0.0
+	current_vel = calc_velocity(move_dir, true, false, false)
+
+	velocity = current_vel
 	move_and_slide()
 	animate()
+	if (name == "SpawnHole16"):
+		print(current_vel, " -> ", velocity, ", Floor: ", on_floor, ", Slope: ", on_slope)
 
 	if (prev_global_position - global_position).length() <= 0.1:
 		if (stuck_timer <= stuck_grace_timer):
@@ -46,15 +55,53 @@ func _on_physics_process(delta):
 		stuck_timer = 0.0
 
 	if ($TurnTimer.time_left <= 0):
-		var wall_contact = (move_dir.x > 0 and ($R1.is_colliding() or $R2.is_colliding())) \
-							or (move_dir.x <= 0 and ($L1.is_colliding() or $L2.is_colliding()))
+		var wall_contact = (move_dir.x > 0 and $RWall.is_colliding() or $RWall2.is_colliding()) \
+							or (move_dir.x <= 0 and $LWall.is_colliding() or $LWall2.is_colliding())
+		var free_opposite_slope = true
+		if (on_floor and on_slope):
+			free_opposite_slope = (move_dir.x > 0 and !$LFloor.is_colliding()) or \
+								(move_dir.x <= 0 and !$RFloor.is_colliding())
+		#if (name == "SpawnHole19"):
+			#print(velocity, " Wall: ", wall_contact, ", Floor: ", on_floor, ", Slope: ", on_slope)
 
-		if wall_contact and !stuck:
+		if wall_contact and free_opposite_slope and !stuck:
 			move_dir *= -1
 			am.play("enemy_jump", self)
 			$TurnTimer.start()
 
 	prev_global_position = global_position
+
+func calc_velocity(move_dir, do_gravity = true, do_acceleration = false, do_friction = false) -> Vector2:
+	var out: = current_vel
+	var fractional_speed = speed
+	if is_in_water:
+		fractional_speed = speed * Vector2(0.666, 0.666)
+
+	#X
+	if do_acceleration:
+		if move_dir.x != 0:
+			out.x = min(abs(out.x) + acceleration, fractional_speed.x)
+			out.x *= move_dir.x
+		elif do_friction:
+			if on_floor:
+				out.x = lerp(out.x, 0.0, ground_cof)
+			else:
+				out.x = lerp(out.x, 0.0, air_cof)
+	else: #no acceleration
+		if (on_floor and on_slope):
+			fractional_speed.x = min(fractional_speed.x, 10.0)
+		out.x = fractional_speed.x * move_dir.x
+
+	#Y
+	if do_gravity:
+		if !on_floor or (on_floor and on_slope):
+			out.y += gravity * get_physics_process_delta_time()
+		else:
+			out.y = 0.0
+		if move_dir.y < 0:
+			out.y = fractional_speed.y * move_dir.y
+
+	return out
 
 func animate():
 	var displacement = (prev_global_position - global_position) / get_physics_process_delta_time()
