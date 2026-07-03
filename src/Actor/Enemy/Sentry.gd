fba99b790d5df3d3c883ef9a64302c786c7a4166
SanpoNeko <sanponeko23@gmail.com>
2026-07-03 09:57:04 +0700

    Sentry: Properly calculate bullet arc according to a waypoint General: All enemy test level now prefix with En

--- src/Actor/Enemy/Sentry.gd
+++ src/Actor/Enemy/Sentry.gd
@@ -6,18 +6,14 @@ const TX_0 = preload("res://assets/Actor/Enemy/Sentry.png")
 
 const HAIRBALL = preload("res://src/Bullet/Enemy/Hairball.tscn")
 
-@export var look_dir: Vector2 = Vector2.LEFT
 @export var cooldown_time = 2.0
-
-@export var projectile_travel_time: float = 0.5
-@export var projectile_speed: int = 150
 @export var projectile_damage: int = 2
-var shoot_distance := 0.0
+
+var shoot_displace_x := 0.0
 var shoot_height := 0.0
 
 var move_dir: Vector2
 
-var target: Node = null
 const shoot_reload_cycle_time := 1.2
 var anim_speed := 1.0
 
@@ -29,10 +25,10 @@ func setup(): #Reminder: no function called can use await
 	speed = Vector2(50, 200)
 	gravity = 250
 	reward = 2
-	$Sprite2D.flip_h = look_dir.x >= 0.0
 	anim_speed = max(1.0, shoot_reload_cycle_time / cooldown_time)
-	shoot_distance = abs($ShootPosition.position.x)
+	shoot_displace_x = $ShootPosition.position.x
 	shoot_height = abs($ShootPosition.position.y)
+	$Sprite2D.flip_h = shoot_displace_x >= 0.0
 
 	w.emit_signal("finished_spawn_entities_step")
 	change_state("idle")
@@ -52,17 +48,18 @@ func prepare_bullet():
 	var bullet_origin := Vector2(0.0, -12.0)
 	bullet.position = global_position + bullet_origin
 
-	bullet.speed = projectile_speed
-	bullet.direction = Vector2(look_dir.x, -1)
+	var peak_displace_y = -shoot_height
+	var target_displace_x = shoot_displace_x
 
-	#var normal_time_travel = 2.0
-	#var target_displace_x = shoot_distance * look_dir.x
-#
-	#var b_speed_x = target_displace_x / normal_time_travel
-	#var b_speed_y = (-0.5 * bullet.gravity * pow(normal_time_travel, 2) - bullet_origin.y) / normal_time_travel
-#
-	#bullet.speed = Vector2(b_speed_x, b_speed_y).length()
-	#bullet.direction = Vector2(b_speed_x, b_speed_y).normalized()
+	var time_peak_to_ground = sqrt(-2.0 * peak_displace_y / bullet.gravity)
+	var time_peak_to_start = sqrt(max((bullet_origin.y - peak_displace_y) * 2.0 / bullet.gravity, 0.0))
+	var time_travel = time_peak_to_ground + time_peak_to_start
+
+	var bullet_vel_x = target_displace_x / time_travel
+	var bullet_vel_y = (-0.5 * bullet.gravity * pow(time_travel, 2) - bullet_origin.y) / time_travel
+
+	bullet.speed = Vector2(bullet_vel_x, bullet_vel_y).length()
+	bullet.direction = Vector2(bullet_vel_x, bullet_vel_y).normalized()
 
 	w.middle.add_child(bullet)
 
@@ -72,11 +69,9 @@ func exit_shoot(next_state):
 
 ### SIGNALS ###
 func _on_PlayerDetector_body_entered(body):
-	target = body.owner
 	change_state("shoot")
 
 func _on_PlayerDetector_body_exited(_body):
-	target = null
 	change_state("idle")
 
 func _on_AnimationPlayer_animation_finished(anim_name: StringName) -> void:
