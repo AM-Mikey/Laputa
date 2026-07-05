c9b2b216ee6e8241451c020b193bee96ea9405e7
SanpoNeko <sanponeko23@gmail.com>
2026-07-05 11:34:22 +0700

    Sentry: Implement basic diff 1, 2, 3 behaviour Sentry: Make a proper test area

--- src/Actor/Enemy/Sentry.gd
+++ src/Actor/Enemy/Sentry.gd
@@ -3,16 +3,22 @@ extends Enemy
 const ICON = preload("res://assets/Actor/Enemy/SentryIcon.png")
 
 const TX_0 = preload("res://assets/Actor/Enemy/Sentry.png")
+const TX_1 = preload("res://assets/Actor/Enemy/Sentry.png") #WIP: Placeholder
+const TX_2 = preload("res://assets/Actor/Enemy/Sentry.png") #WIP: Placeholder
+const TX_3 = preload("res://assets/Actor/Enemy/Sentry.png") #WIP: Placeholder
 
 const HAIRBALL = preload("res://src/Bullet/Enemy/Hairball.tscn")
 
+@export var difficulty: int = 0
 @export var cooldown_time = 2.0
 @export var projectile_damage: int = 2
 
-var shoot_displace_x := 0.0
-var shoot_height := 0.0
+var facing_right := false:
+	set(val):
+		$Sprite2D.flip_h = !val
+		facing_right = val
 
-var move_dir: Vector2
+var target: Node2D = null
 
 const shoot_reload_cycle_time := 1.2
 var anim_speed := 1.0
@@ -26,14 +32,25 @@ func setup(): #Reminder: no function called can use await
 	gravity = 250
 	reward = 2
 	anim_speed = max(1.0, shoot_reload_cycle_time / cooldown_time)
-	if (get_node_or_null("ShootWaypoint")):
-		shoot_displace_x = $ShootWaypoint.position.x
-		shoot_height = abs($ShootWaypoint.position.y)
-	$Sprite2D.flip_h = shoot_displace_x >= 0.0
 
-	if (get_node_or_null("VURect")):
-		$PlayerDetector/CollisionShape2D.shape.size = $VURect.value.size
-		$PlayerDetector/CollisionShape2D.position = $VURect.value.size / 2.0 + $VURect.value.position
+	facing_right = $ShootWaypoint.position.x >= 0.0
+
+	$PlayerDetector/CollisionShape2D.shape.size = $VURect.value.size
+	$PlayerDetector/CollisionShape2D.position = $VURect.value.size / 2.0 + $VURect.value.position
+
+	match difficulty:
+		0:
+			$Sprite2D.texture = TX_0
+			$Sprite2D.self_modulate = Color.WHITE #WIP: Placeholder
+		1:
+			$Sprite2D.texture = TX_1
+			$Sprite2D.self_modulate = Color.GREEN #WIP: Placeholder
+		2:
+			$Sprite2D.texture = TX_2
+			$Sprite2D.self_modulate = Color.BLUE #WIP: Placeholder
+		3:
+			$Sprite2D.texture = TX_3
+			$Sprite2D.self_modulate = Color.YELLOW #WIP: Placeholder
 
 	w.emit_signal("finished_spawn_entities_step")
 	change_state("idle")
@@ -45,18 +62,42 @@ func enter_shoot(_last_state):
 	else:
 		ap.play("Shoot", -1.0, anim_speed)
 
+func do_shoot(_delta):
+	if difficulty in [1, 2] and target:
+		facing_right = target.global_position.x - global_position.x >= 0.0
+
 func prepare_bullet():
-	am.play("enemy_shoot", self)
+	if difficulty == 2 && !target:
+		return
+
 	var bullet = HAIRBALL.instantiate()
 	bullet.damage = projectile_damage
 
 	var bullet_origin := Vector2(0.0, -12.0)
 	bullet.position = global_position + bullet_origin
 
-	var peak_displace_y = -shoot_height
-	var target_displace_x = shoot_displace_x
+	var peak_displace_y: float = 0.0
+	var target_displace_x: float = 0.0
 
-	var time_peak_to_ground = sqrt(-2.0 * peak_displace_y / bullet.gravity)
+	match difficulty:
+		0:
+			peak_displace_y = $ShootWaypoint.position.y
+			target_displace_x = $ShootWaypoint.position.x
+		1:
+			peak_displace_y = $ShootWaypoint.position.y
+			target_displace_x = abs($ShootWaypoint.position.x)
+			target_displace_x *= 1.0 if facing_right else -1.0
+		2:
+			peak_displace_y = $ShootWaypoint.position.y
+			target_displace_x = target.global_position.x - global_position.x
+
+	if peak_displace_y >= 0.0:
+		if difficulty == 2:
+			peak_displace_y = -16.0 * 5.0
+		else:
+			peak_displace_y = -16.0
+
+	var time_peak_to_ground = sqrt(max(-2.0 * peak_displace_y / bullet.gravity, 0.0))
 	var time_peak_to_start = sqrt(max((bullet_origin.y - peak_displace_y) * 2.0 / bullet.gravity, 0.0))
 	var time_travel = time_peak_to_ground + time_peak_to_start
 
@@ -67,16 +108,18 @@ func prepare_bullet():
 	bullet.direction = Vector2(bullet_vel_x, bullet_vel_y).normalized()
 
 	w.middle.add_child(bullet)
+	am.play("enemy_shoot", self)
 
-
-func exit_shoot(next_state):
+func exit_shoot(_next_state):
 	$ShootDelay.stop()
 
 ### SIGNALS ###
 func _on_PlayerDetector_body_entered(body):
+	target = body
 	change_state("shoot")
 
 func _on_PlayerDetector_body_exited(_body):
+	target = null
 	change_state("idle")
 
 func _on_AnimationPlayer_animation_finished(anim_name: StringName) -> void:
