21b1754f29d64f18eca22b4ef66385d8164b9887
AM-Mikey <76826262+AM-Mikey@users.noreply.github.com>
2026-07-06 23:25:22 -0500

    command and mission fixes
    fixed free commands
    many mission and command fixes, loading fixes

--- src/World.gd
+++ src/World.gd
@@ -159,7 +159,7 @@ func change_level_via_code(level_path, use_save_data):
 	if use_save_data:
 		SaveSystem.read_level_data_from_temp(current_level)
 	await get_tree().physics_frame
-	await get_tree().physics_frame #wait for npcs to spawn, takes 2 frames for some reason
+	await get_tree().physics_frame #wait for npcs to spawn, takes 2 frames for some reason ##is it read_player_data from save related?
 	if use_save_data:
 		SaveSystem.read_dialog_data_from_temp(current_level)
 	setup_missions(use_save_data, "code")
@@ -234,6 +234,7 @@ func setup_missions(use_save_data: bool, type = "first_time"):
 		ms.setup_level_from_mission_progress_history()
 	else:
 		var update_conversations = true if type in ["first_time", "code"] else false
+		var is_entering = true
 
 		if type != "trigger": #Don't if we're moving thru a door or loadzone
 			if current_level.debug_main_mission_stage_name: #add all stages until we get to the one that matches.
@@ -245,10 +246,10 @@ func setup_missions(use_save_data: bool, type = "first_time"):
 						if s[0] == current_level.debug_main_mission_stage_name:
 							ms.main_mission_stage = s
 							main_array_completed = true
-				ms.setup_level_from_array(main_array, update_conversations)
+				ms.setup_level_from_array(main_array, update_conversations, is_entering)
 			else: #start with the very first main mission stage
 				ms.main_mission_stage = ms.MAIN_MISSION[0]
-				ms.update_level_via_mission("Main", "current", update_conversations)
+				ms.update_level_via_mission("Main", "current", update_conversations, is_entering)
 		else: #trigger
 			var main_array = []
 			var main_array_completed = false
@@ -258,7 +259,7 @@ func setup_missions(use_save_data: bool, type = "first_time"):
 					if s[0] == ms.main_mission_stage[0]:
 						ms.main_mission_stage = s
 						main_array_completed = true
-			ms.setup_level_from_array(main_array, update_conversations)
+			ms.setup_level_from_array(main_array, update_conversations, is_entering)
 
 
 		#side missions
@@ -274,7 +275,7 @@ func setup_missions(use_save_data: bool, type = "first_time"):
 						side_array.append([m.resource_path.get_file().trim_suffix(".tres"), s[0]])
 						if s[0] == m.current_stage:
 							side_array_completed = true
-		ms.setup_level_from_array(side_array, update_conversations)
+		ms.setup_level_from_array(side_array, update_conversations, is_entering)
 		ms.mission_progress_check()
 
 
