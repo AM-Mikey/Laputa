7516b8768d947d7667cab4fb441298f02baad901
SanpoNeko <sanponeko23@gmail.com>
2026-08-03 11:28:33 +0700

    Crusher: Implement proper breakable prop interaction

--- src/Actor/Enemy/Crusher.gd
+++ src/Actor/Enemy/Crusher.gd
@@ -31,7 +31,7 @@ var facing_dir: FacingDir = FacingDir.NEUTRAL
 
 var debug_path_color := Color.RED
 var debug_path_start := true
-var debug_name = ["Crusher4"]
+var debug_name = ["Crusher120"]
 var debug_self_rect: Rect2 = Rect2()
 var debug_body_rect: Rect2 = Rect2()
 var debug_over_rect: Rect2 = Rect2()
@@ -97,8 +97,9 @@ func setup():
 	w.emit_signal("finished_spawn_entities_step")
 
 	await w.finished_spawning
-	for node in get_tree().get_nodes_in_group("Props"):
-		$Standable.add_collision_exception_with(node)
+	for node in get_tree().get_nodes_in_group("PhysicsProps"):
+		if !node.has_node("BreakArea"): #Ignore all non-breakable prop
+			$Standable.add_collision_exception_with(node)
 
 func _physics_process(delta: float) -> void:
 	if disabled || dead:
@@ -145,7 +146,11 @@ func _physics_process(delta: float) -> void:
 
 func on_crush_body_entered(body):
 	if !nearby_bodies.has(body):
-		nearby_bodies.append(body)
+		if body.get_collision_layer_value(5):
+			if body.has_node("BreakArea"):
+				nearby_bodies.append(body)
+		else:
+			nearby_bodies.append(body)
 
 func on_crush_body_exited(body):
 	if nearby_bodies.has(body):
@@ -166,96 +171,139 @@ func crush_check():
 		if body.get_collision_layer_value(1):
 			body = body.get_parent()
 
-		var body_collision_shape = body.get_node("CollisionShape2D")
-		if !body_collision_shape or body_collision_shape.disabled:
-			continue
+		var body_is_breakable_prop = body.get_collision_layer_value(5)
 
-		var body_size = body_collision_shape.shape.get_rect().size
-		var body_rect := Rect2(body_collision_shape.global_position - body_size / 2.0, body_size)
-		var body_overlap_rect := body_rect.intersection(crush_rect)
+		if body_is_breakable_prop:
+			var raycheck_param: PhysicsRayQueryParameters2D = PhysicsRayQueryParameters2D.new()
+			raycheck_param.collision_mask = 8 #World / Prop
+			raycheck_param.hit_from_inside = true
+			raycheck_param.collide_with_bodies = true
+			raycheck_param.collide_with_areas = false
 
-		if name in debug_name:
-			debug_self_rect = crush_rect
-			debug_body_rect = body_rect
-			debug_over_rect = body_overlap_rect
-			queue_redraw()
+			var move_dir_check_list := []
+			if moving_direction.x != 0.0: move_dir_check_list.append(Vector2(moving_direction.x, 0.0))
+			if moving_direction.y != 0.0: move_dir_check_list.append(Vector2(0.0, moving_direction.y))
 
-		if body_overlap_rect != Rect2():
-			var body_position := body_overlap_rect.get_center()
-			var check_direction := Vector2.ZERO
-			var raycheck_position := [Vector2.ZERO, Vector2.ZERO, Vector2.ZERO]
+			for curr_move_direction in move_dir_check_list:
+				var checked_bodies := []
+				var body_queue := [body]
+				var success := false
+				while !body_queue.is_empty():
+					var curr_body = body_queue.pop_back()
+					var curr_body_collision_shape = curr_body.get_node_or_null("CollisionShape2D")
+					if !curr_body_collision_shape or curr_body_collision_shape.disabled:
+						continue
 
-			const body_in_y_tolerance: = 3.0
-			var body_in_y = body_position.y >= crush_rect.position.y + body_in_y_tolerance && body_position.y <= crush_rect.position.y + crush_rect.size.y - body_in_y_tolerance
-			const body_in_x_tolerance: = 3.0
-			var body_in_x = body_position.x >= crush_rect.position.x + body_in_x_tolerance && body_position.x <= crush_rect.position.x + crush_rect.size.x - body_in_x_tolerance
+					var curr_body_size = curr_body_collision_shape.shape.get_rect().size
+					var root_check_position = curr_body.global_position + curr_body_size * 0.5 + curr_move_direction * curr_body_size * 0.5
+					for check_direction in [curr_move_direction.rotated(-PI / 2.0), curr_move_direction.rotated(PI / 2.0)]:
+						raycheck_param.from = root_check_position + check_direction * (curr_body_size - Vector2.ONE) * 0.5
+						raycheck_param.to = raycheck_param.from + curr_move_direction * 0.5
+						raycheck_param.exclude = [curr_body.get_rid(), $Standable.get_rid()]
+						var collision = physics_space.intersect_ray(raycheck_param)
+						if collision:
+							var collider = collision["collider"]
+							if collider is TileMapLayer || (collider.get_collision_layer_value(4) && !collider.has_node("BreakArea")):
+								success = true
+								body.on_break()
+								break
+							elif collider.has_node("BreakArea"):
+								if !checked_bodies.has(collider):
+									body_queue.append(collider)
+					checked_bodies.append(curr_body)
+					if success: break
+				if success: break
+		else:
+			var body_collision_shape = body.get_node("CollisionShape2D")
+			if !body_collision_shape or body_collision_shape.disabled:
+				continue
 
-			#if name in debug_name:
-				#print("Find raycheck")
-			const adjust := 1.0
-			const adjust_2 := 3.0
-			if body_in_y and moving_direction.x < 0.0 and body_position.x < crush_rect_center.x:
-				check_direction.x = -1 - adjust_2
-				raycheck_position.append(body_rect.position + Vector2(adjust_2, adjust))
-				raycheck_position.append(body_rect.position + Vector2(adjust_2, body_rect.size.y / 2.0))
-				raycheck_position.append(body_rect.position + Vector2(adjust_2, body_rect.size.y - adjust))
-			elif body_in_y and moving_direction.x > 0.0 and body_position.x > crush_rect_center.x:
-				check_direction.x = 1 + adjust_2
-				raycheck_position.append(body_rect.position + Vector2(body_rect.size.x - adjust_2, adjust))
-				raycheck_position.append(body_rect.position + Vector2(body_rect.size.x - adjust_2, body_rect.size.y / 2.0))
-				raycheck_position.append(body_rect.position + Vector2(body_rect.size.x - adjust_2, body_rect.size.y - adjust))
-			if body_in_x and moving_direction.y < 0.0 and body_position.y < crush_rect_center.y:
-				check_direction.y = -1 - adjust_2
-				raycheck_position.append(body_rect.position + Vector2(adjust, adjust_2))
-				raycheck_position.append(body_rect.position + Vector2(body_rect.size.x / 2.0, adjust_2))
-				raycheck_position.append(body_rect.position + Vector2(body_rect.size.x - adjust, adjust_2))
-			elif body_in_x and moving_direction.y > 0.0 and body_position.y > crush_rect_center.y:
-				check_direction.y = 1 + adjust_2
-				raycheck_position.append(body_rect.position + Vector2(adjust, body_rect.size.y - adjust_2))
-				raycheck_position.append(body_rect.position + Vector2(body_rect.size.x / 2.0, body_rect.size.y - adjust_2))
-				raycheck_position.append(body_rect.position + Vector2(body_rect.size.x - adjust, body_rect.size.y - adjust_2))
+			var body_size = body_collision_shape.shape.get_rect().size
+			var body_rect := Rect2(body_collision_shape.global_position - body_size / 2.0, body_size)
+			var body_overlap_rect := body_rect.intersection(crush_rect)
 
-			if check_direction == Vector2.ZERO: continue
+			if name in debug_name:
+				debug_self_rect = crush_rect
+				debug_body_rect = body_rect
+				debug_over_rect = body_overlap_rect
+				queue_redraw()
 
-			#if name in debug_name:
-				#print(check_direction)
+			if body_overlap_rect != Rect2():
+				var body_position := body_overlap_rect.get_center()
+				var check_direction := Vector2.ZERO
+				var raycheck_position := [Vector2.ZERO, Vector2.ZERO, Vector2.ZERO]
 
-			var collide_with_world: bool = false
-			for i in range(0, raycheck_position.size()):
-				var raycheck_param: PhysicsRayQueryParameters2D = PhysicsRayQueryParameters2D.new()
-				raycheck_param.from = raycheck_position[i]
-				raycheck_param.to = raycheck_position[i] + check_direction
-				raycheck_param.exclude = [body.get_rid(), $Standable.get_rid()]
-				raycheck_param.collision_mask = 8
-				raycheck_param.hit_from_inside = true
-				raycheck_param.collide_with_bodies = true
-				raycheck_param.collide_with_areas = false
+				const body_in_y_tolerance: = 3.0
+				var body_in_y = body_position.y >= crush_rect.position.y + body_in_y_tolerance && body_position.y <= crush_rect.position.y + crush_rect.size.y - body_in_y_tolerance
+				const body_in_x_tolerance: = 3.0
+				var body_in_x = body_position.x >= crush_rect.position.x + body_in_x_tolerance && body_position.x <= crush_rect.position.x + crush_rect.size.x - body_in_x_tolerance
 
-				var collision = physics_space.intersect_ray(raycheck_param)
-				while !collision.is_empty():
-					var collider = collision["collider"]
-					if collider.is_in_group("MovingPlatform"):
-						var collider_move_direction = collider.get_parent().moving_direction
-						if moving_direction.x * collider_move_direction.x < 0.0 || moving_direction.y * collider_move_direction.y < 0.0:
+				#if name in debug_name:
+					#print("Find raycheck")
+				const adjust := 1.0
+				const adjust_2 := 3.0
+				if body_in_y and moving_direction.x < 0.0 and body_position.x < crush_rect_center.x:
+					check_direction.x = -1 - adjust_2
+					raycheck_position.append(body_rect.position + Vector2(adjust_2, adjust))
+					raycheck_position.append(body_rect.position + Vector2(adjust_2, body_rect.size.y / 2.0))
+					raycheck_position.append(body_rect.position + Vector2(adjust_2, body_rect.size.y - adjust))
+				elif body_in_y and moving_direction.x > 0.0 and body_position.x > crush_rect_center.x:
+					check_direction.x = 1 + adjust_2
+					raycheck_position.append(body_rect.position + Vector2(body_rect.size.x - adjust_2, adjust))
+					raycheck_position.append(body_rect.position + Vector2(body_rect.size.x - adjust_2, body_rect.size.y / 2.0))
+					raycheck_position.append(body_rect.position + Vector2(body_rect.size.x - adjust_2, body_rect.size.y - adjust))
+				if body_in_x and moving_direction.y < 0.0 and body_position.y < crush_rect_center.y:
+					check_direction.y = -1 - adjust_2
+					raycheck_position.append(body_rect.position + Vector2(adjust, adjust_2))
+					raycheck_position.append(body_rect.position + Vector2(body_rect.size.x / 2.0, adjust_2))
+					raycheck_position.append(body_rect.position + Vector2(body_rect.size.x - adjust, adjust_2))
+				elif body_in_x and moving_direction.y > 0.0 and body_position.y > crush_rect_center.y:
+					check_direction.y = 1 + adjust_2
+					raycheck_position.append(body_rect.position + Vector2(adjust, body_rect.size.y - adjust_2))
+					raycheck_position.append(body_rect.position + Vector2(body_rect.size.x / 2.0, body_rect.size.y - adjust_2))
+					raycheck_position.append(body_rect.position + Vector2(body_rect.size.x - adjust, body_rect.size.y - adjust_2))
+
+				if check_direction == Vector2.ZERO: continue
+
+				#if name in debug_name:
+					#print(check_direction)
+
+				var collide_with_world: bool = false
+				for i in range(0, raycheck_position.size()):
+					var raycheck_param: PhysicsRayQueryParameters2D = PhysicsRayQueryParameters2D.new()
+					raycheck_param.from = raycheck_position[i]
+					raycheck_param.to = raycheck_position[i] + check_direction
+					raycheck_param.exclude = [body.get_rid(), $Standable.get_rid()]
+					raycheck_param.collision_mask = 8
+					raycheck_param.hit_from_inside = true
+					raycheck_param.collide_with_bodies = true
+					raycheck_param.collide_with_areas = false
+
+					var collision = physics_space.intersect_ray(raycheck_param)
+					while !collision.is_empty():
+						var collider = collision["collider"]
+						if collider.is_in_group("MovingPlatform"):
+							var collider_move_direction = collider.get_parent().moving_direction
+							if moving_direction.x * collider_move_direction.x < 0.0 || moving_direction.y * collider_move_direction.y < 0.0:
+								collide_with_world = true
+								break
+							else:
+								var exception = raycheck_param.exclude
+								exception.append(collider.get_rid())
+								raycheck_param.exclude = exception
+								collision = physics_space.intersect_ray(raycheck_param)
+						else:
+							#if name in debug_name:
+								#print("A")
+								#print(check_direction, " ", collider.name)
 							collide_with_world = true
 							break
-						else:
-							var exception = raycheck_param.exclude
-							exception.append(collider.get_rid())
-							raycheck_param.exclude = exception
-							collision = physics_space.intersect_ray(raycheck_param)
-					else:
-						#if name in debug_name:
-							#print("A")
-							#print(check_direction, " ", collider.name)
-						collide_with_world = true
+					if collide_with_world:
 						break
-				if collide_with_world:
-					break
 
-			if collide_with_world:
-				body.hit(999, Vector2.ZERO)
-				body.die() # Pierce invis
+				if collide_with_world:
+					body.hit(999, Vector2.ZERO)
+					body.die() # Pierce invis
 
 @onready var ap = $AnimationPlayer
 func animate():
