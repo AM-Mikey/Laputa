802b39a6cf7a7caa569550a34083633158abed70
SanpoNeko <sanponeko23@gmail.com>
2026-08-14 09:56:17 +0700

    Shield: Implement basic work for difficulty == 1

--- src/Actor/Enemy/Shield.gd
+++ src/Actor/Enemy/Shield.gd
@@ -3,7 +3,10 @@ extends Enemy
 const ICON = preload("res://assets/Actor/Enemy/ShieldIcon.png")
 
 const TX_0 = preload("res://assets/Actor/Enemy/Shield.png")
+const TX_1 = preload("res://assets/Actor/Enemy/Shield.png")
+const TX_2  = preload("res://assets/Actor/Enemy/Shield.png")
 
+@export var difficulty: int = 0
 var move_dir: = Vector2.LEFT: set = set_move_dir
 var defend_time: = 0.7
 
@@ -19,6 +22,18 @@ func setup(): #Reminder: no function called can use await
 	move_dir = $MoveDir.direction.snappedf(1.0)
 	$PlayerDetection/CollisionShape2D.shape.size = $PlayerDetectArea.value.size
 	$PlayerDetection/CollisionShape2D.position = $PlayerDetectArea.value.position + $PlayerDetectArea.value.size / 2.0
+	$AttackDetection.monitoring = difficulty >= 1
+	match difficulty:
+		0:
+			$Sprite2D.texture = TX_0
+		1:
+			$Sprite2D.texture = TX_1
+			$Sprite2D.self_modulate = Color.GREEN
+		2:
+			$Sprite2D.texture = TX_2
+			$Sprite2D.self_modulate = Color.RED
+
+
 	state = "idle" #Prevent standng still when instantly see player on spawn
 	w.emit_signal("finished_spawn_entities_step")
 
@@ -44,13 +59,21 @@ func do_walk(_delta):
 	move_and_slide()
 
 func enter_defend(_last_state):
-	ap.play("IdleShield")
-	$StateTimer.start(defend_time)
-	await $StateTimer.timeout
-	change_state("walk")
+	if difficulty == 0:
+		ap.play("IdleShield")
+		$StateTimer.start(defend_time)
+		await $StateTimer.timeout
+		change_state("walk")
 
-### HELPER ###
+func do_defend(_delta):
+	if difficulty >= 1:
+		velocity = calc_velocity(move_dir)
+		move_and_slide()
 
+func enter_attack(_prev_state):
+	ap.play("Attack")
+
+### SETTER ###
 func set_move_dir(dir):
 	move_dir = dir
 	#if move_dir.x * shield_dir.x >= 0.0:
@@ -65,31 +88,38 @@ func set_move_dir(dir):
 	$BulletBlocker/StaticBody2D/Right.set_deferred("disabled", move_dir == Vector2.LEFT)
 	$Hurtbox/Left.set_deferred("disabled", move_dir == Vector2.LEFT)
 	$Hurtbox/Right.set_deferred("disabled", move_dir != Vector2.LEFT)
-	#else:
-		#if state == "walk":
-			#ap.play("WalkCake")
-		#elif state == "wait":
-			#ap.play("IdleCake")
-		#$BulletBlocker/Left.set_deferred("disabled", true)
-		#$BulletBlocker/Right.set_deferred("disabled", true)
-		#$BulletBlocker/StaticBody2D/Left.set_deferred("disabled", true)
-		#$BulletBlocker/StaticBody2D/Right.set_deferred("disabled", true)
-		#$Hurtbox/Left.set_deferred("disabled", false)
-		#$Hurtbox/Right.set_deferred("disabled", false)
+	$AttackDetection.scale.x = -signf(move_dir.x)
+	if $AttackDetection.scale.x == 0.0: $AttackDetection.scale.x = 1.0
+	$AttackHitbox.scale.x = -signf(move_dir.x)
+	if $AttackHitbox.scale.x == 1.0: $AttackHitbox.scale.x = 1.0
 
+### UTILITY ###
+func enable_attack_hitbox(val: bool):
+	$AttackHitbox.monitorable = val
+	$AttackHitbox.monitoring = val
 
-
+func play_sound(sfx_name: String):
+	am.play(sfx_name)
 
 ### SIGNALS ###
 func _on_BulletBlocker_body_entered(body):
-	if body.get_collision_layer_value(7): #bullet
+	_on_BulletBlocker_area_entered(body)
+
+func _on_BulletBlocker_area_entered(_area):
+	if (difficulty == 0) || \
+		(difficulty == 1 && state == "walk"):
 		change_state("defend")
 
-func _on_BulletBlocker_area_entered(area):
-	if area.get_collision_layer_value(7): #bullet
-		change_state("defend")
-
-func _on_PlayerDetection_body_entered(body: Node2D) -> void:
+func _on_PlayerDetection_body_entered(_body):
 	if state == "idle":
-		$PlayerDetection.monitoring = false
+		$PlayerDetection.set_deferred("monitoring", false)
 		change_state("walk")
+
+func _on_AttackDetection_body_entered(_body):
+	if difficulty == 1 and state in ["walk", "defend"]:
+		change_state("attack")
+
+func _on_AnimationPlayer_animation_finished(anim_name: StringName) -> void:
+	if difficulty == 1:
+		if anim_name == "Attack":
+			change_state("walk")
