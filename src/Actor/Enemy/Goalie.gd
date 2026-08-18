0c7b62b7828eb122ba444a360de801cfcc7bb4ae
sanponeko <sanponeko23@gmail.com>
2026-08-18 07:23:49 +0700

    Setting up thing for GOalie

---
+++ src/Actor/Enemy/Goalie.gd
@@ -0,0 +1,45 @@
+extends Enemy
+
+const ICON = preload("res://assets/Actor/Enemy/GoalieThumbnail.png")
+
+const TX_0 = preload("res://assets/Actor/Enemy/Goalie.png")
+
+@export var jump_height: int = 6
+@export var cooldown_time = 1
+var kick_damage = 4
+
+var move_dir = Vector2.ZERO
+var target = null
+@onready var start_pos = position
+var jump_pos
+
+
+func _ready():
+	hp = 4
+	damage_on_contact = 1
+	speed = Vector2(100, 200)
+	gravity = 250
+
+	reward = 3
+
+
+func _on_ActiveDetector_body_entered(body):
+	if $StateMachine.current_state == $StateMachine/Idle:
+		$StateMachine.change_state("Active")
+
+
+func _on_ActiveDetector_body_exited(_body):
+	if $StateMachine.current_state == $StateMachine/Active:
+		$StateMachine.change_state("Idle")
+
+
+func _on_JumpDetector_body_entered(_body):
+	if $StateMachine.current_state == $StateMachine/Active:
+		$StateMachine.change_state("Rise")
+
+
+func _on_KickDectector_body_entered(body):
+	if not Engine.is_editor_hint():
+		if $StateMachine.current_state == $StateMachine/Rise:
+			target = body
+			$StateMachine.change_state("Kick")
