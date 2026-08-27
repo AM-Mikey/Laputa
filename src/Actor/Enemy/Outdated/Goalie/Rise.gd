625cd546cf008a83d3a02480a26b09c5c69e41d3
sanponeko <sanponeko23@gmail.com>
2026-08-27 09:43:32 +0700

    Goalie: Now no longer stuck after rising and bonk on the ceiling

--- src/Actor/Enemy/Outdated/Goalie/Rise.gd
+++ src/Actor/Enemy/Outdated/Goalie/Rise.gd
@@ -4,7 +4,7 @@ extends State
 @onready var ap = em.get_node("AnimationPlayer")
 
 func state_process():
-	if em.position.y <= em.jump_pos.y or !em.target or em.position.y <= em.target.global_position.y:
+	if em.is_on_ceiling() or em.position.y <= em.jump_pos.y or !em.target or em.position.y <= em.target.global_position.y:
 		sm.change_state("fall")
 		return
 	em.velocity = calc_velocity()
