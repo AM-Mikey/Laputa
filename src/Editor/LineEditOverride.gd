36ffeef5de45abe99b9c9522b6c18955af38bddd
FlowerLover32 <118894576+FlowerLover32@users.noreply.github.com>
2026-08-28 15:21:37 +0300

    Fix ctrl+c for LineEditOverride
    Additionally, fixed how c works with selection

--- src/Editor/LineEditOverride.gd
+++ src/Editor/LineEditOverride.gd
@@ -2,8 +2,18 @@ extends LineEdit
 
 func _gui_input(event):
 	if event.is_action_pressed("ui_cancel"):
-		if event.shift_pressed:
-			insert_text_at_caret("C")
-		else:
-			insert_text_at_caret("c")
-		accept_event()
+		if is_editing():
+			if event.ctrl_pressed:
+				if has_selection():
+					DisplayServer.clipboard_set(get_selected_text())
+			else:
+				if has_selection():
+					var from := get_selection_from_column()
+					var length := get_selection_to_column() - from
+					text = text.erase(from, length)
+					caret_column = from
+				if event.shift_pressed:
+					insert_text_at_caret("C")
+				else:
+					insert_text_at_caret("c")
+			accept_event()
