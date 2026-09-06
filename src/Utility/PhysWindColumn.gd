678f69d91ef439e0e8110a00a2e65355fe91675c
SanpoNeko <sanponeko23@gmail.com>
2026-09-06 11:20:23 +0700

    Fan: Fix a bug where the fan particle is emitted despite being blocked at point-blank

--- src/Utility/PhysWindColumn.gd
+++ src/Utility/PhysWindColumn.gd
@@ -27,7 +27,7 @@ func update():
 	$BodyDetector/CollisionShape2D.shape.size = column_rect.size
 	$BodyDetector/CollisionShape2D.global_position = column_rect.position + (column_rect.size / 2.0)
 	if effect:
-		effect.tile_distance = (max(column_rect.size.x, column_rect.size.y) / 16.0)
+		effect.tile_distance = (column_rect.size * wind_dir).length() / 16.0
 		effect.direction = wind_dir
 		effect.global_position = global_position
 		effect.update()
