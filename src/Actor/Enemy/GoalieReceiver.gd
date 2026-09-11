a33d3c3fb9b4226a381bc787d7fb8eaa557d3bc3
SanpoNeko <sanponeko23@gmail.com>
2026-09-11 09:48:07 +0700

    GoalieReceiver: Will not kick itself or its friend Goalie: Adjust aim calculation

--- src/Actor/Enemy/GoalieReceiver.gd
+++ src/Actor/Enemy/GoalieReceiver.gd
@@ -1,9 +1,8 @@
 extends EnemyGoalie
 
-var kick_next_state = ""
+var kick_next_state: = ""
+var kick_force: = 300.0
 
-func _setup():
-	pass
 
 ### STATES ###
 func enter_kick(prev_state):
@@ -40,7 +39,8 @@ func _on_KickHitbox_area_entered(area: Area2D) -> void:
 	elif area.get_collision_layer_value(17): #playerhurt
 		area.get_parent().hit(kick_damage, Vector2(80 * look_dir.x, 0), kick_hitbox)
 	elif area.get_collision_layer_value(9): #breakable
-		area.get_parent().on_break("cut")
+		if area.name == "BreakArea":
+			area.get_parent().on_break("cut")
 
 
 
@@ -49,14 +49,15 @@ func _on_KickHitbox_body_entered(body: Node2D) -> void:
 		kick_hitbox.set_deferred("monitoring", false)
 		kick_hitbox.set_deferred("monitorable", false)
 	elif body.get_collision_layer_value(2): #enemy
+		if body is EnemyGoalie: return
 		var player = f.pc()
 		if !player: return
-		var dir: = global_position.direction_to(player.global_position)
-		body.hit(0.0, Vector2.ZERO, kick_hitbox, dir, 200.0)
+		var dir: = body.global_position.direction_to(player.global_position + Vector2(0, -30))
+		body.hit(0.0, Vector2.ZERO, kick_hitbox, dir, kick_force)
 	elif body.get_collision_layer_value(7) || body.get_collision_layer_value(14):
 		var player = f.pc()
 		if !player: return
 		var tween = body.create_tween()
-		var dir: = global_position.direction_to(player.global_position)
-		tween.tween_property(body, "velocity", dir * 200.0, 0.1)
+		var dir: = body.global_position.direction_to(player.global_position + Vector2(0, -30))
+		tween.tween_property(body, "velocity", dir * kick_force, 0.1)
 		tween.tween_interval(3.0)
